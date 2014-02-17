20ff139f9af4697bc96709105105aefbc25ff304
Eric Leblond <eric@regit.org>
2014-02-17 10:40:11 +0100

    rules: use wrapper to display link to generator

--- rules/views.py
+++ rules/views.py
@@ -35,6 +35,10 @@ from forms import *
 if settings.USE_ELASTICSEARCH:
     from elasticsearch import *
 
+def scirius_render(request, template, context):
+    context['generator'] = settings.RULESET_MIDDLEWARE
+    return render(request, template, context)
+
 def listing(request, objectname, name):
     # FIXME could be improved by generating function name
     assocfn = { 'Sources': SourceTable, 'Categories': CategoryTable, 'Rulesets': RulesetTable }
@@ -53,7 +57,7 @@ def listing(request, objectname, name):
         context['action'] = objectname.__name__.lower()
     except:
         pass
-    return render(request, 'rules/object_list.html', context)
+    return scirius_render(request, 'rules/object_list.html', context)
 
 # Create your views here.
 def index(request):
@@ -61,7 +65,7 @@ def index(request):
     source_list = Source.objects.all().order_by('-created_date')[:5]
     context = {'ruleset_list': ruleset_list,
                 'source_list': source_list}
-    return render(request, 'rules/index.html', context)
+    return scirius_render(request, 'rules/index.html', context)
 
 def sources(request):
     return listing(request, Source, 'Sources')
@@ -71,7 +75,7 @@ def source(request, source_id):
     cats = CategoryTable(Category.objects.filter(source = source))
     tables.RequestConfig(request).configure(cats)
     context = {'source': source, 'categories': cats}
-    return render(request, 'rules/source.html', context)
+    return scirius_render(request, 'rules/source.html', context)
 
 def categories(request):
     return listing(request, Category, 'Categories')
@@ -82,7 +86,7 @@ def category(request, cat_id):
     tables.RequestConfig(request).configure(rules)
     category_path = [ cat.source ]
     context = {'category': cat, 'rules': rules, 'object_path': category_path}
-    return render(request, 'rules/category.html', context)
+    return scirius_render(request, 'rules/category.html', context)
 
 def rule(request, rule_id, key = 'pk'):
     if request.is_ajax():
@@ -105,7 +109,7 @@ def rule(request, rule_id, key = 'pk'):
         elif refer.key == 'bugtraq':
             refer.url = "http://www.securityfocus.com/bid/" + refer.value
     context = {'rule': rule, 'references': references, 'object_path': rule_path}
-    return render(request, 'rules/rule.html', context)
+    return scirius_render(request, 'rules/rule.html', context)
 
 def update_source(request, source_id):
     source = get_object_or_404(Source, pk=source_id)
@@ -125,12 +129,12 @@ def add_source(request):
                         )
                 source.save()
             except IntegrityError, error:
-                return render(request, 'rules/add_source.html', { 'form': form, 'error': error })
+                return scirius_render(request, 'rules/add_source.html', { 'form': form, 'error': error })
             return redirect(source)
     else:
         form = SourceForm() # An unbound form
 
-    return render(request, 'rules/add_source.html', { 'form': form, })
+    return scirius_render(request, 'rules/add_source.html', { 'form': form, })
 
 def rulesets(request):
     return listing(request, Ruleset, 'Rulesets')
@@ -159,7 +163,7 @@ def ruleset(request, ruleset_id, mode = 'struct'):
         response = HttpResponse(file_content, content_type="text/plain")
         response['Content-Disposition'] = 'attachment; filename=scirius.rules'
         return response
-    return render(request, 'rules/ruleset.html', context)
+    return scirius_render(request, 'rules/ruleset.html', context)
 
 def add_ruleset(request):
     if request.method == 'POST': # If the form has been submitted...
@@ -170,12 +174,12 @@ def add_ruleset(request):
             try:
                 ruleset = form.create_ruleset()
             except IntegrityError, error:
-                return render(request, 'rules/add_ruleset.html', { 'form': form, 'error': error })
+                return scirius_render(request, 'rules/add_ruleset.html', { 'form': form, 'error': error })
             return redirect(ruleset)
     else:
         form = RulesetForm() # An unbound form
 
-    return render(request, 'rules/add_ruleset.html', { 'form': form, })
+    return scirius_render(request, 'rules/add_ruleset.html', { 'form': form, })
 
 def update_ruleset(request, ruleset_id):
     ruleset = get_object_or_404(Ruleset, pk=ruleset_id)
@@ -221,7 +225,7 @@ def edit_ruleset(request, ruleset_id):
         context = {'ruleset': ruleset, 'categories_list': categories_list, 'sources': sources, 'rules': rules, 'cats_selection': ", ".join(cats_selection) }
         if request.GET.has_key('mode'):
                 context['mode'] = request.GET['mode']
-        return render(request, 'rules/edit_ruleset.html', context)
+        return scirius_render(request, 'rules/edit_ruleset.html', context)
 
 def ruleset_add_supprule(request, ruleset_id):
     ruleset = get_object_or_404(Ruleset, pk=ruleset_id)
@@ -231,7 +235,7 @@ def ruleset_add_supprule(request, ruleset_id):
             rules = EditRuleTable(Rule.objects.filter(content__icontains=request.POST['search']))
             tables.RequestConfig(request).configure(rules)
             context = { 'ruleset': ruleset, 'rules': rules }
-            return render(request, 'rules/search_rule.html', context)
+            return scirius_render(request, 'rules/search_rule.html', context)
         elif request.POST.has_key('rule_selection'):
             for rule in request.POST.getlist('rule_selection'):
                 rule_object = get_object_or_404(Rule, pk=rule)
@@ -239,7 +243,7 @@ def ruleset_add_supprule(request, ruleset_id):
             ruleset.save()
         return redirect(ruleset)
     context = { 'ruleset': ruleset }
-    return render(request, 'rules/search_rule.html', context)
+    return scirius_render(request, 'rules/search_rule.html', context)
 
 def delete_ruleset(request, ruleset_id):
     ruleset = get_object_or_404(Ruleset, pk=ruleset_id)
@@ -248,7 +252,7 @@ def delete_ruleset(request, ruleset_id):
         return redirect("/rules/ruleset/")
     else:
         context = {'object': ruleset, 'delfn': 'delete_ruleset' }
-        return render(request, 'rules/delete.html', context)
+        return scirius_render(request, 'rules/delete.html', context)
 
 def copy_ruleset(request, ruleset_id):
     ruleset = get_object_or_404(Ruleset, pk=ruleset_id)
@@ -260,4 +264,4 @@ def copy_ruleset(request, ruleset_id):
     else:
         form = RulesetCopyForm()
     context = {'object': ruleset , 'form': form}
-    return render(request, 'rules/copy_ruleset.html', context)
+    return scirius_render(request, 'rules/copy_ruleset.html', context)
