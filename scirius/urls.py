fd3d20e0d7c53e0125aed9c0b99a51510eba2935
Eric Leblond <eric@regit.org>
2014-05-04 21:32:51 +0200

    scirius: access to index redirect to rules index

--- scirius/urls.py
+++ scirius/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 from django.contrib import admin
+
+from views import homepage
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rules/', include('rules.urls')),
     url(r'^'+ settings.RULESET_MIDDLEWARE + '/', include('' + settings.RULESET_MIDDLEWARE + '.urls')),
+    url('^$', homepage),
 )
