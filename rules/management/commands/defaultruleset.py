916fb3d7c7e8b31ef6dcef090819ad465c8b04ee
Eric Leblond <eric@regit.org>
2014-05-29 13:29:43 +0200

    rules: add two management commands
    This patch adds two management commands: 'addsource' and
    'defaultruleset'. The idea is to be able to create from
    command line objects in the database to have a ready to
    use scirius (for example in the case of SELKS).
    
    'addsource' can be used to add and update a source. Its syntax is as
    follow:
     python manage.py addsource "ETOpen Ruleset" https://rules.emergingthreats.net/open/suricata-2.0.1/emerging.rules.tar.gz http sigs
    
    'defaultruleset' will create a ruleset using all defined sources and
    taking all categories available in the sources. The syntax is as
    follow:
    
     python manage.py defaultruleset "ETOpen based ruleset"

---
+++ rules/management/commands/defaultruleset.py
@@ -0,0 +1,50 @@
+"""
+Copyright(C) 2014, Stamus Networks
+Written by Eric Leblond <eleblond@stamus-networks.com>
+
+This file is part of Scirius.
+
+Scirius is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Scirius is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Scirius.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+from rules.models import Ruleset, SourceAtVersion, Category
+from datetime import datetime
+
+class Command(BaseCommand):
+    args = 'name'
+    help = 'Create a ruleset and populate it with rules from existing sources'
+
+    def handle(self, *args, **options):
+        name = args[0]
+        try:
+            sourceat = SourceAtVersion.objects.all()
+        except:
+            raise CommandError("No SourceAtVersion is defined")
+        try:
+            categories = Category.objects.all()
+        except:
+            raise CommandError("No Category is defined")
+        ruleset = Ruleset.objects.create(
+            name = name,
+            created_date = datetime.now(),
+            updated_date = datetime.now()
+            )
+        for source in sourceat:
+            ruleset.sources.add(source)
+        for cat in categories:
+            ruleset.categories.add(cat)
+        ruleset.save()
+        self.stdout.write('Successfully created default ruleset "%s"' % name)
+
