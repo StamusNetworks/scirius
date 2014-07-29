afa58f53832cbea69438ee620ce76c594e4d3254
Eric Leblond <eric@regit.org>
2014-07-29 23:00:15 +0200

    rules: add disable category command
    This new command can be used to disable on category in a ruleset:
     python manage.py disablecategory "ET based Ruleset" emerging-inappropriate

---
+++ rules/management/commands/disablecategory.py
@@ -0,0 +1,46 @@
+"""
+Copyright(C) 2014, Stamus Networks
+Written by Eric Leblond <eleblond@stamus-networks.com>
+
+This file is part of Scirius.
+
+Scirius is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Scirius is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Scirius.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+from rules.models import Ruleset, SourceAtVersion, Category
+from datetime import datetime
+
+class Command(BaseCommand):
+    args = 'ruleset category'
+    help = 'Remove a category from a ruleset'
+
+    def handle(self, *args, **options):
+        name = args[0]
+        catname = args[1]
+        
+        try:
+            ruleset = Ruleset.objects.filter(name = name)
+            ruleset = ruleset[0]
+        except:
+            raise CommandError("No ruleset with name '%s' is defined" % (name))
+        try:
+            categories = Category.objects.filter(name = catname)
+        except:
+            raise CommandError("No Category is defined")
+        for cat in categories:
+            ruleset.categories.remove(cat)
+        ruleset.save()
+        self.stdout.write('Successfully removed "%s" from ruleset "%s"' % (catname, name))
+
