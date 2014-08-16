c8f8fc6670f6ebe9f36c211bfdffd37af7536c4a
Eric Leblond <eric@regit.org>
2014-08-16 23:34:24 +0200

    suricata: add south migrations to git

---
+++ suricata/migrations/0001_initial.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Suricata'
+        db.create_table(u'suricata_suricata', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
+            ('descr', self.gf('django.db.models.fields.CharField')(max_length=400)),
+            ('created_date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('updated_date', self.gf('django.db.models.fields.DateTimeField')(blank=True)),
+            ('ruleset', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['rules.Ruleset'], blank=True)),
+        ))
+        db.send_create_signal(u'suricata', ['Suricata'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Suricata'
+        db.delete_table(u'suricata_suricata')
+
+
+    models = {
+        u'rules.category': {
+            'Meta': {'object_name': 'Category'},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 2, 17, 0, 0)'}),
+            'descr': ('django.db.models.fields.CharField', [], {'max_length': '400', 'blank': 'True'}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['rules.Source']"})
+        },
+        u'rules.reference': {
+            'Meta': {'object_name': 'Reference'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '1000'})
+        },
+        u'rules.rule': {
+            'Meta': {'object_name': 'Rule'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['rules.Category']"}),
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '10000'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'msg': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'references': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['rules.Reference']", 'symmetrical': 'False', 'blank': 'True'}),
+            'rev': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'sid': ('django.db.models.fields.IntegerField', [], {'default': '0', 'unique': 'True'}),
+            'state': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        u'rules.ruleset': {
+            'Meta': {'object_name': 'Ruleset'},
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['rules.Category']", 'symmetrical': 'False', 'blank': 'True'}),
+            'created_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'descr': ('django.db.models.fields.CharField', [], {'max_length': '400', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'sources': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['rules.SourceAtVersion']", 'symmetrical': 'False'}),
+            'suppressed_rules': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['rules.Rule']", 'symmetrical': 'False', 'blank': 'True'}),
+            'updated_date': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'})
+        },
+        u'rules.source': {
+            'Meta': {'object_name': 'Source'},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'datatype': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'updated_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'uri': ('django.db.models.fields.CharField', [], {'max_length': '400'})
+        },
+        u'rules.sourceatversion': {
+            'Meta': {'object_name': 'SourceAtVersion'},
+            'git_version': ('django.db.models.fields.CharField', [], {'default': "'HEAD'", 'max_length': '42'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['rules.Source']"}),
+            'updated_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 2, 17, 0, 0)', 'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '42'})
+        },
+        u'suricata.suricata': {
+            'Meta': {'object_name': 'Suricata'},
+            'created_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'descr': ('django.db.models.fields.CharField', [], {'max_length': '400'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'ruleset': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['rules.Ruleset']", 'blank': 'True'}),
+            'updated_date': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['suricata']
