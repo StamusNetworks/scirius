440ab7f0e083a88bc55be1f41bd0ff4753cf34f1
Eric Leblond <eric@regit.org>
2014-08-17 11:08:05 +0200

    rules: factorize javascript code
    Put common JS functions in scirius.js file and load this file in
    the base template.

---
+++ rules/static/js/scirius.js
@@ -0,0 +1,30 @@
+$( 'document' ).ready(function() {
+    $(".msg").click(function( event ) {
+        if ($(this).find(".detail").length) {
+            $(this).find(".detail").slideUp(
+                function() {
+                    $(this).remove();
+                }
+            );
+        } else {
+            sid = $( this ).parent().find("a").html();
+            $.ajax(
+                {
+                        type:"GET",
+                        url:"/rules/rule/"+sid,
+                        success: function(data) {
+                            mylink = $('a').filter(function(index) { return $(this).text() == data.sid; });
+                            mytd = mylink.parent().parent().find(".msg");
+                            mytd.append("<div class='detail'>" + data.content + "</div>");
+                            mytd.find(".detail").slideDown();
+                        },
+                }
+            );
+        }
+    });
+});
+
+
+function toggle_scirius_menu(img) {
+    $("#app_menu").toggle(duration=200)
+}
