9ba41268a493bfe1932f45fbe575bd775253cbd0
Eric Leblond <eric@regit.org>
2014-08-21 22:46:24 +0200

    rules: add search form in header
    This form allows to search for all elements matching the string.

--- rules/views.py
+++ rules/views.py
@@ -63,6 +63,35 @@ def about(request):
         pass
     return scirius_render(request, 'rules/about.html', context)
 
+def search(request):
+    context = {}
+    if request.method == 'POST':
+        if request.POST.has_key('search'):
+            length = 0
+            rules = Rule.objects.filter(content__icontains=request.POST['search'])
+            if len(rules) > 0:
+                length += len(rules)
+                rules = RuleTable(rules)
+                tables.RequestConfig(request).configure(rules)
+            else:
+                rules = None
+            categories = Category.objects.filter(name__icontains=request.POST['search'])
+            if len(categories) > 0:
+                length += len(categories)
+                categories = CategoryTable(categories)
+                tables.RequestConfig(request).configure(categories)
+            else:
+                categories = None
+            rulesets = Ruleset.objects.filter(name__icontains=request.POST['search'])
+            if len(rulesets) > 0:
+                length += len(rulesets)
+                rulesets = RulesetTable(rulesets)
+                tables.RequestConfig(request).configure(rulesets)
+            else:
+                rulesets = None
+            context = { 'rules': rules, 'categories': categories, 'rulesets': rulesets, 'motif': request.POST['search'], 'length': length }
+    return scirius_render(request, 'rules/search.html', context)
+
 def sources(request):
     return scirius_listing(request, Source, 'Sources')
 
