c3bf4752625c4e64ebc3f6c9d711b88df3e926bb
Eric Leblond <eric@regit.org>
2014-11-05 13:35:27 +0100

    accounts: display superuser info

--- accounts/forms.py
+++ accounts/forms.py
@@ -28,4 +28,4 @@ class LoginForm(forms.Form):
 class UserSettingsForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'is_active']
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_active', 'is_superuser']
--- accounts/tables.py
+++ accounts/tables.py
@@ -28,4 +28,4 @@ class UserTable(tables.Table):
     username = tables.LinkColumn('user', args=[tables.A('pk')])
     class Meta(DefaultMeta):
         model = User
-        fields = ("username", "first_name", "last_name", "email", "is_active")
+        fields = ("username", "first_name", "last_name", "email", "is_superuser", "is_active")
