af9fd29905592593a8089012a2ffe4043efc8f9f
Eric Leblond <eric@regit.org>
2015-05-19 20:58:07 +0200

    rules: add prefix to influxdb request
    If a different prefix than suricata is used in suri-graphite we
    may have to adjust.

--- rules/influx.py
+++ rules/influx.py
@@ -22,7 +22,7 @@ along with Scirius.  If not, see <http://www.gnu.org/licenses/>.
 from django.conf import settings
 from influxdb import InfluxDBClient
 
-def influx_get_timeline(time_range, request="eve_rate"):
+def influx_get_timeline(time_range, request="eve_rate", probe_prefix="suricata"):
     result = None
     client = InfluxDBClient(settings.INFLUXDB_HOST, settings.INFLUXDB_PORT,
                             settings.INFLUXDB_USER, settings.INFLUXDB_PASSWORD,
@@ -30,9 +30,9 @@ def influx_get_timeline(time_range, request="eve_rate"):
     if request == "eve_rate":
         result = client.query("select mean(value) from /eve.*.rate_1m/ where time > now()-%ds group by time(%ds) fill(0) order asc"  % (time_range, time_range / 120))
     elif request == "suri_packet":
-        result = client.query("select derivative(value) from /suricata.*.capture.kernel_packets/ where time > now()-%ds group by time(%ds) fill(0) order asc" % (time_range, time_range / 120))
+        result = client.query("select derivative(value) from /%s.*.capture.kernel_packets/ where time > now()-%ds group by time(%ds) fill(0) order asc" % (probe_prefix, time_range, time_range / 120))
     elif request == "suri_drop":
-        result = client.query("select derivative(value) from /suricata.*.capture.kernel_drops/ where time > now()-%ds group by time(%ds) fill(0) order asc" % (time_range, time_range / 120))
+        result = client.query("select derivative(value) from /%s.*.capture.kernel_drops/ where time > now()-%ds group by time(%ds) fill(0) order asc" % (probe_prefix, time_range, time_range / 120))
     # speed is always positive so set to 0 if negative (which is mostly due to restart of stats)
     for line in result:
         line['points'] = [[pt[0], max(pt[1], 0)] for pt in line['points']]
