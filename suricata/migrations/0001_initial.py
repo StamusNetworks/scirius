9743aa78eb140e6f35820ab3815c334dbb2dd37e
Eric Leblond <eric@regit.org>
2015-08-24 00:23:48 +0200

    suricata: add recent django migration
    Add django > 1.6 migration to the sources to guarantee an easy
    upgrade for users using these django versions.
    
    This fixes #48.

---
+++ suricata/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import suricata.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rules', '0041_source_authkey'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Suricata',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=100, validators=[suricata.models.validate_hostname])),
+                ('descr', models.CharField(max_length=400)),
+                ('output_directory', models.CharField(max_length=400)),
+                ('created_date', models.DateTimeField(verbose_name=b'date created')),
+                ('updated_date', models.DateTimeField(verbose_name=b'date updated', blank=True)),
+                ('ruleset', models.ForeignKey(blank=True, to='rules.Ruleset', null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
