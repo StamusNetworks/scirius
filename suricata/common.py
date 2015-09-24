7bcd5d9a80e968f6db0acf82a653d6d373eb094f
Eric Leblond <eric@regit.org>
2015-09-24 13:39:10 +0200

    suricata: fix suricata test for old suricata

--- suricata/common.py
+++ suricata/common.py
@@ -59,6 +59,11 @@ logging:
   - console:
       enabled: yes
       type: json
+action-order:
+  - pass
+  - drop
+  - reject
+  - alert
 vars:
   address-groups:
     HOME_NET: "[192.168.0.0/16,10.0.0.0/8,172.16.0.0/12]"
