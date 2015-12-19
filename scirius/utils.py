7ef028a0b8656a147d3cb1106a5d9c0a877f5808
Eric Leblond <eric@regit.org>
2015-12-19 21:55:47 +0100

    scirius: export es2x variable
    It can be used in all pages to select Elasticsearch 2.0 compatible
    code.

--- scirius/utils.py
+++ scirius/utils.py
@@ -44,6 +44,10 @@ def scirius_render(request, template, context):
             else:
                 context['kibana_url'] = settings.KIBANA_URL
             context['kibana_version'] = settings.KIBANA_VERSION
+    if settings.ELASTICSEARCH_2X:
+        context['es2x'] = 1
+    else:
+        context['es2x'] = 0
     return render(request, template, context)
 
 def scirius_listing(request, objectname, name, template = 'rules/object_list.html', table = None, adduri = None):
