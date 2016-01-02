cb5aff93707b1711de099339cc9f992ce47f87ec
Eric Leblond <eric@regit.org>
2016-01-02 01:07:25 +0100

    scirius: add proxy capability to kibana 4

--- scirius/urls.py
+++ scirius/urls.py
@@ -13,6 +13,10 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('accounts.urls')),
     url(r'^'+ settings.RULESET_MIDDLEWARE + '/', include('' + settings.RULESET_MIDDLEWARE + '.urls')),
     url('^$', homepage),
+    # Forward "app/kibana.*" to kibana (work around to https://github.com/elastic/kibana/issues/5230)
+    url(r'^(?P<path>app/kibana.*)$', KibanaProxyView.as_view()),
+    # Forward timelion plugin
+    url(r'^(?P<path>timelion/.*)$', KibanaProxyView.as_view()),
     url(r'^kibana/(?P<path>.*)$', KibanaProxyView.as_view()),
     url(r'^elasticsearch/(?P<path>.*)$', ElasticsearchProxyView.as_view()),
 )
