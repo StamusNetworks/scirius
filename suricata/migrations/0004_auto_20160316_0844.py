d55aaf9a4f63a49a18276fb432521f306c1b1813
Eric Leblond <eric@regit.org>
2016-03-16 09:45:24 +0100

    suricata: add migration
    Altering name create a migration so let's add it.

---
+++ suricata/migrations/0004_auto_20160316_0844.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('suricata', '0003_suricata_yaml_file'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='suricata',
+            name='output_directory',
+            field=models.CharField(max_length=400, verbose_name=b'Rules directory'),
+        ),
+        migrations.AlterField(
+            model_name='suricata',
+            name='yaml_file',
+            field=models.CharField(max_length=400, verbose_name=b'Suricata configuration file'),
+        ),
+    ]
