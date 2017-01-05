50571f749b6ad4b586af43a0122dc6e59cb62181
Laurent Defert <lds@stamus-networks.com>
2017-01-05 16:37:46 +0100

    scirius: remove admin modules url

--- scirius/urls.py
+++ scirius/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
-from django.contrib import admin
-
 from views import homepage, KibanaProxyView, EveboxProxyView
 
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^rules/', include('rules.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^'+ settings.RULESET_MIDDLEWARE + '/', include('' + settings.RULESET_MIDDLEWARE + '.urls')),
