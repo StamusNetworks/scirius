ad0b52390443722f89d0fbc1ac588c1b29da5445
Eric Leblond <eric@regit.org>
2017-08-23 23:05:44 +0200

    scirius: next release is 1.2.5

--- scirius/settings.py
+++ scirius/settings.py
@@ -15,7 +15,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SCIRIUS_FLAVOR="Scirius CE"
-SCIRIUS_VERSION="1.2.4"
+SCIRIUS_VERSION="1.2.5"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
