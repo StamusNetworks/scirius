bdb01f4eaff88184ddb6b06b42f406e3395e2b6b
Laurent Defert <lds@stamus-networks.com>
2018-02-08 12:02:11 +0100

    rules: add an initial rest rules and ruleset api

--- rules/rest_api.py
+++ rules/rest_api.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from rest_framework import serializers, viewsets, permissions
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 from rest_framework.routers import DefaultRouter
 
-from rules.models import Ruleset
+from rules.models import Rule, Ruleset
 
 
 class ModelSerializer(serializers.ModelSerializer):
@@ -24,7 +26,10 @@ class ModelSerializer(serializers.ModelSerializer):
 class RulesetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ruleset
-        fields = ('name',)
+        fields = ('pk', 'name', 'descr', 'created_date', 'updated_date', 'need_test', 'validity', \
+                'errors', 'rules_count')
+        read_only_fields = ('pk', 'created_date', 'updated_date', 'need_test', 'validity', 'errors', \
+                'rules_count')
 
 
 class RulesetViewSet(viewsets.ModelViewSet):
@@ -32,5 +37,41 @@ class RulesetViewSet(viewsets.ModelViewSet):
     serializer_class = RulesetSerializer
 
 
+class RuleChangeSerializer(serializers.Serializer):
+    ruleset = serializers.PrimaryKeyRelatedField(queryset=Ruleset.objects.all(), write_only=True)
+    comment = serializers.CharField(required=False, allow_blank=True, write_only=True)
+
+
+class RuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rule
+        fields = ('pk', 'sid', 'category', 'msg', 'state', 'state_in_source', 'rev', 'content', \
+                'flowbits', 'imported_date', 'updated_date')
+
+
+class RuleViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Rule.objects.all()
+    serializer_class = RuleSerializer
+
+    @detail_route(methods=['post'])
+    def enable(self, request, pk):
+        rule = self.get_object()
+        serializer = RuleChangeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        rule.enable(serializer.validated_data['ruleset'], request.user,
+                serializer.validated_data.get('comment', None))
+        return Response({'enable': 'ok'})
+
+    @detail_route(methods=['post'])
+    def disable(self, request, pk):
+        rule = self.get_object()
+        serializer = RuleChangeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        rule.disable(serializer.validated_data['ruleset'], request.user,
+                serializer.validated_data.get('comment', None))
+        return Response({'enable': 'ok'})
+
+
 router = DefaultRouter()
 router.register('rules/ruleset', RulesetViewSet)
+router.register('rules/rule', RuleViewSet)
--- rules/tests.py
+++ rules/tests.py
@@ -18,11 +18,14 @@ You should have received a copy of the GNU General Public License
 along with Scirius.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
-
 from django.utils import timezone
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-from models import Source, SourceAtVersion, Category
+from models import Category, Rule, Ruleset, Source, SourceAtVersion
 
 import tempfile
 from shutil import rmtree
@@ -45,3 +48,79 @@ class SourceCreationTestCase(TestCase):
         self.source.update()
         self.assertEqual(len(SourceAtVersion.objects.filter(source = self.source)), 1)
         self.assertNotEqual(len(Category.objects.filter(source = self.source)), 0)
+
+
+class RestAPITestBase(object):
+    def setUp(self):
+        self.user = User.objects.create(username='scirius', password='scirius', is_superuser=True, is_staff=True)
+        self.client.force_login(self.user)
+
+    def _make_request(self, method, url, *args, **kwargs):
+        func = getattr(self.client, method)
+        http_status = kwargs.pop('status', status.HTTP_200_OK)
+        kwargs['format'] = 'json'
+        response = func(url, *args, **kwargs)
+        self.assertEqual(response.status_code, http_status, 'Request failed: \n%s %s\n%s %s\n%s' %
+                (method.upper(), url, response.status_code, response.reason_phrase, response))
+        return getattr(response, 'data', None)
+
+    http_get = lambda self, *args, **kwargs: self._make_request('get', *args, **kwargs)
+    http_post = lambda self, *args, **kwargs: self._make_request('post', *args, **kwargs)
+    http_put = lambda self, *args, **kwargs: self._make_request('put', *args, **kwargs)
+    http_patch = lambda self, *args, **kwargs: self._make_request('patch', *args, **kwargs)
+
+
+class RestAPIRuleTestCase(RestAPITestBase, APITestCase):
+    def setUp(self):
+        RestAPITestBase.setUp(self)
+        APITestCase.setUp(self)
+
+        self.source = Source.objects.create(name='test source', created_date=timezone.now(),
+                method='local', datatype='sig')
+        self.source.save()
+        self.source_at_version = SourceAtVersion.objects.create(source=self.source, version='42')
+        self.source_at_version.save()
+        self.category = Category.objects.create(name='test category', filename='test',
+                source=self.source)
+        self.category.save()
+        self.rule = Rule.objects.create(sid=1, category=self.category, msg='test rule',
+                content='test rule')
+        self.rule.save()
+        self.ruleset = Ruleset.objects.create(name='test ruleset', descr='descr', created_date=timezone.now(),
+                updated_date=timezone.now())
+        self.ruleset.save()
+        self.ruleset.sources.add(self.source_at_version)
+        self.ruleset.categories.add(self.category)
+
+    def test_001_rule_detail(self):
+        self.http_get(reverse('rule-detail', args=(self.rule.pk,)))
+
+    def test_002_rule_disable(self):
+        self.http_post(reverse('rule-disable', args=(self.rule.pk,)), {'ruleset': self.ruleset.pk})
+        self.ruleset.refresh_from_db()
+
+        disabled = self.ruleset.suppressed_rules.all()
+        self.assertEqual(len(disabled), 1)
+        self.assertEqual(disabled[0].pk, self.rule.pk)
+
+        self.http_post(reverse('rule-enable', args=(self.rule.pk,)), {'ruleset': self.ruleset.pk})
+        self.ruleset.refresh_from_db()
+
+        disabled = self.ruleset.suppressed_rules.all()
+        self.assertEqual(len(disabled), 0)
+
+    def test_003_rule_permission(self):
+        self.client.logout()
+
+        # Non logged request are redirected to login page
+        self.http_post(reverse('rule-disable', args=(self.rule.pk,)), {'ruleset': self.ruleset.pk},
+                status=status.HTTP_302_FOUND)
+
+        # Post not authorized non-staff
+        self.user.is_superuser = False
+        self.user.is_staff = False
+        self.user.save()
+        self.client.force_login(self.user)
+        # Read still authorized
+        self.http_post(reverse('rule-disable', args=(self.rule.pk,)), {'ruleset': self.ruleset.pk},
+                status=status.HTTP_403_FORBIDDEN)
