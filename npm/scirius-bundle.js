c28afaca9a33bf74c7fd37a90933fc6b24834140
Eric Leblond <eric@regit.org>
2018-02-09 11:43:58 +0100

    rules: add some components to webpack bundle

--- npm/scirius-bundle.js
+++ npm/scirius-bundle.js
@@ -1,6 +1,12 @@
 // CSS
+import "../rules/static/rules/bootstrap.min.css";
 import "../rules/static/rules/nv.d3.min.css";
 import "../rules/static/rules/static.css";
 import "../node_modules/patternfly/dist/css/patternfly.css";
 import "../node_modules/patternfly/dist/css/patternfly-additions.css";
 //import "../scss/_styles.scss";
+// JS
+import "../rules/static/js/bootstrap.min.js";
+import "../rules/static/js/tipsy.js";
+import "../rules/static/js/scirius.js";
+
