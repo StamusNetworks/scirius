a86a5a099456bad8e6d75bc90dd2f9b496acd0b2
Eric Leblond <eric@regit.org>
2018-02-28 14:47:36 +0100

    rules: set version in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='scirius',
-    version='1.2.0',
+    version='2.0',
     packages=['scirius','rules','suricata', 'accounts', 'viz'],
     scripts=['manage.py'],
     include_package_data=True,
