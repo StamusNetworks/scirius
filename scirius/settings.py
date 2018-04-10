20255e9c0f6c27ffd60d021de49a1a77e3274380
Laurent Defert <lds@stamus-networks.com>
2018-04-10 09:24:36 +0200

    rules: enable REST SearchFilter module
    Provides searches through a `search` query parameters, that supports some operators:
    - ^ Starts-with search.
    - = Exact matches.
    - $ Regex search.
    
    See:
    http://www.django-rest-framework.org/api-guide/filtering/#searchfilter

--- scirius/settings.py
+++ scirius/settings.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
