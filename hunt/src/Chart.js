1ee08bf4a65eb650892be021bb6eb3bc18e7ca73
Eric Leblond <eric@regit.org>
2018-05-13 14:47:24 +0200

    hunt: optimize graph refresh

--- hunt/src/Chart.js
+++ hunt/src/Chart.js
@@ -3,8 +3,12 @@ import 'c3/c3.css';
 
 export class SciriusChart extends C3Chart {
   componentWillReceiveProps(newProps) {
-          newProps.axis.x.max = Date.now();
-          this.chart = undefined;
+          var range = this.chart.axis.min();
+          range.x = newProps.from_date;
+          this.chart.axis.min(range);
+          range = this.chart.axis.max();
+          range.x = Date.now();
+          this.chart.axis.max(range);
           this.updateChart(newProps);
   }
 }
