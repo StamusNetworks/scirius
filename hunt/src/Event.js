d8ad7f04a9d5983e7fc03886e7493e351ab42a7a
Eric Leblond <eric@regit.org>
2018-06-09 23:46:58 +0200

    hunt: move field action to the value

--- hunt/src/Event.js
+++ hunt/src/Event.js
@@ -2,27 +2,38 @@ import React from 'react';
 import { Icon } from 'patternfly-react';
 
 export class EventField extends React.Component {
+   render() {
+      return(
+           <React.Fragment>
+               <dt>{this.props.field_name}</dt>
+	       <dd>
+	           <EventValue field={this.props.field} value={this.props.value} addFilter={this.props.addFilter}/>
+	       </dd>
+           </React.Fragment>
+	   )
+   }
+}
+
+export class EventValue extends React.Component {
     constructor(props) {
        super(props);
        this.state = {display_actions: false };
     }
 
-   render() {
-      return(
-           <React.Fragment>
-               <dt>{this.props.field_name}</dt>
-	       <dd
-	           onMouseOver={e => {this.setState({display_actions: true})}}
-	           onMouseOut={e => {this.setState({display_actions: false})}}
-	       >{this.props.value}
+    render() {
+        return(
+	    <div
+	        onMouseOver={e => {this.setState({display_actions: true})}}
+	        onMouseOut={e => {this.setState({display_actions: false})}}
+	       >
+	       {this.props.value}
 	             {this.state.display_actions &&
                      <React.Fragment>
 		         <a onClick={ e => {this.props.addFilter(this.props.field, this.props.value, false)}}> <Icon type="fa" name="search-plus"/></a>
 		         <a onClick={ e => {this.props.addFilter(this.props.field, this.props.value, true)}}> <Icon type="fa" name="search-minus"/></a>
                      </React.Fragment>
 		     }
-	       </dd>
-           </React.Fragment>
-	   )
-   }
+	    </div>
+	)
+    }
 }
