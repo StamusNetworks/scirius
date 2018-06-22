7e5ff5e283cac8d1703b6537868da94925884be2
Eric Leblond <eric@regit.org>
2018-06-22 23:56:10 +0200

    hunt: fix filter over on chrome

--- hunt/src/Event.js
+++ hunt/src/Event.js
@@ -27,12 +27,10 @@ export class EventValue extends React.Component {
 	        onMouseOut={e => {this.setState({display_actions: false})}}
 	       >
 	       {this.props.value}
-	             {this.state.display_actions &&
-                     <React.Fragment>
+                     <span className={this.state.display_actions ? 'eventFilters' : 'eventFiltersHidden'} >
 		         <a onClick={ e => {this.props.addFilter(this.props.field, this.props.value, false)}}> <Icon type="fa" name="search-plus"/></a>
 		         <a onClick={ e => {this.props.addFilter(this.props.field, this.props.value, true)}}> <Icon type="fa" name="search-minus"/></a>
-                     </React.Fragment>
-		     }
+                     </span>
                 {this.props.right_info && 
 		    <span className="pull-right">{this.props.right_info}</span>
                 }
