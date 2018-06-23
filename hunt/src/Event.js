7860ee67a88fd86bb5e524a1a1da7e0d9cc3ef6e
Eric Leblond <eric@regit.org>
2018-06-23 17:46:05 +0200

    hunt: integration with Onyphe

--- hunt/src/Event.js
+++ hunt/src/Event.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Icon } from 'patternfly-react';
+import axios from 'axios';
+import { Icon, Modal, Button, Col } from 'patternfly-react';
+import { ONYPHE_API_KEY } from './config/Onyphe.js'
 
 export class EventField extends React.Component {
    render() {
@@ -14,14 +16,211 @@ export class EventField extends React.Component {
    }
 }
 
+class EventIPGeoloc extends React.Component {
+	render() {
+		return(
+			<Col md={6}>
+			        <h4>Geo localization result</h4>
+				<dl>
+				<dt>Country</dt><dd>{this.props.data.country_name}</dd>
+				{this.props.data.city &&
+				<React.Fragment>
+					<dt>City</dt><dd>{this.props.data.city}</dd>
+				</React.Fragment>
+				}
+				<dt>Organization</dt><dd>{this.props.data.organization}</dd>
+				<dt>ASN</dt><dd>{this.props.data.asn}</dd>
+				<dt>Subnet</dt><dd>{this.props.data.subnet}</dd>
+			</dl>
+			</Col>
+		)
+	}
+}
+
+class EventIPDatascan extends React.Component {
+	render() {
+		return(
+			<Col md={6}>
+			        <h4>Data scanner result</h4>
+				<dl>
+				{this.props.data.product &&
+				<React.Fragment>
+					<dt>Product</dt><dd>{this.props.data.product}</dd>
+				</React.Fragment>
+				}
+				{this.props.data.productversion &&
+				<React.Fragment>
+					<dt>Version</dt><dd>{this.props.data.productversion}</dd>
+				</React.Fragment>
+				}
+				{this.props.data.port &&
+				<React.Fragment>
+					<dt>Port</dt><dd>{this.props.data.port}</dd>
+				</React.Fragment>
+				}
+				{this.props.data.seen_date &&
+				<React.Fragment>
+					<dt>Seen date</dt><dd>{this.props.data.seen_date}</dd>
+				</React.Fragment>
+				}
+				</dl>
+			</Col>
+		)
+	}
+}
+
+class EventIPSynscan extends React.Component {
+	render() {
+		return(
+			<Col md={6}>
+			        <h4>SYN scanner result</h4>
+				<dl>
+				{this.props.data.os &&
+				<React.Fragment>
+					<dt>Operating System</dt><dd>{this.props.data.os}</dd>
+				</React.Fragment>
+				}
+				{this.props.data.port &&
+				<React.Fragment>
+					<dt>Port</dt><dd>{this.props.data.port}</dd>
+				</React.Fragment>
+				}
+				{this.props.data.seen_date &&
+				<React.Fragment>
+					<dt>Seen date</dt><dd>{this.props.data.seen_date}</dd>
+				</React.Fragment>
+				}
+				</dl>
+			</Col>
+		)
+	}
+}
+
+class EventIPThreatlist extends React.Component {
+	render() {
+		return(
+			<Col md={6}>
+			        <h4>Threat list info</h4>
+				<dl>
+				{this.props.data["@type"] &&
+				<React.Fragment>
+					<dt>Type</dt><dd>{this.props.data["@type"]}</dd>
+				</React.Fragment>
+				}
+				{this.props.data.threatlist &&
+				<React.Fragment>
+					<dt>Threat list</dt><dd>{this.props.data.threatlist}</dd>
+				</React.Fragment>
+				}
+				{this.props.data.subnet &&
+				<React.Fragment>
+					<dt>Subnet</dt><dd>{this.props.data.subnet}</dd>
+				</React.Fragment>
+				}
+				{this.props.data.seen_date &&
+				<React.Fragment>
+					<dt>Seen date</dt><dd>{this.props.data.seen_date}</dd>
+				</React.Fragment>
+				}
+				</dl>
+			</Col>
+		)
+	}
+}
+
+class EventIPInfo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { ipinfo: null, show_ip_info: false };
+		this.displayIPInfo = this.displayIPInfo.bind(this);
+		this.closeIPInfo = this.closeIPInfo.bind(this);
+	}
+
+	closeIPInfo() {
+		this.setState({show_ip_info: false});
+	}
+
+	displayIPInfo() {
+		this.setState({show_ip_info: true});
+		if (this.state.ipinfo === null) {
+			axios.get("https://www.onyphe.io/api/ip/" + this.props.value + "?apikey=" + ONYPHE_API_KEY).then(
+				res => {
+					console.log(res.data['results']);
+					this.setState({ipinfo: res.data['results']});	
+				}
+			);
+		}
+		
+	}
+
+	render() {
+		return(
+			<React.Fragment>
+			<a onClick={this.displayIPInfo}>
+				<Icon type="fa" name="info-circle"/>
+			</a>
+			<Modal show={this.state.show_ip_info} onHide={this.closeIPInfo}>
+				<Modal.Header>
+					      <button
+					        className="close"
+					        onClick={this.closeIPInfo}
+					        aria-hidden="true"
+					        aria-label="Close"
+					      >
+       						 <Icon type="pf" name="close" />
+     				 		</button>
+					<Modal.Title>
+						Some Info from Onyphe.io
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					{this.state.ipinfo &&
+						this.state.ipinfo.map( item => {
+							if (item["@category"] === "geoloc") {
+								return(<EventIPGeoloc data={item}/>);
+							}
+							if (item["@category"] === "datascan") {
+								return(<EventIPDatascan data={item}/>);
+							}
+							if (item["@category"] === "synscan") {
+								return(<EventIPSynscan data={item}/>);
+							}
+							if (item["@category"] === "threatlist") {
+								return(<EventIPThreatlist data={item}/>);
+							}
+							return null;
+					})}
+					{this.state.ipinfo === null &&
+					    <p>Fetching IP info</p>
+					}
+				</Modal.Body>
+			    <Modal.Footer>
+			      <Button
+			        bsStyle="default"
+			        className="btn-cancel"
+			        onClick={this.closeIPInfo}
+			      >
+			        Close
+			      </Button>
+			    </Modal.Footer>
+			</Modal>
+			</React.Fragment>
+		)
+	}
+}
+
 class EventValueInfo extends React.Component {
     render() {
 	if (['src_ip', 'dest_ip', 'alert.source.ip', 'alert.target.ip'].indexOf(this.props.field) > -1 ) {
-		return(
-			<a href={"https://www.onyphe.io/search/?query=" + this.props.value} target="_blank">
-				<Icon type="fa" name="info-circle"/>
-			</a>
-		);
+		if (ONYPHE_API_KEY) {
+			return(<EventIPInfo value={this.props.value} />);
+		} else {
+			return(
+				<a href={"https://www.onyphe.io/search/?query=" + this.props.value} target="_blank">
+					<Icon type="fa" name="info-circle"/>
+				</a>
+			);
+		}
 	} 
 	return null;
     }
