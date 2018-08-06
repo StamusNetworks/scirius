c8f1e3ddb7938e76168c4cfbe85734089c2b1373
Eric Leblond <eric@regit.org>
2018-08-06 22:12:16 +0200

    hunt: add license to files

--- hunt/src/Notifications.js
+++ hunt/src/Notifications.js
@@ -1,3 +1,24 @@
+/*
+Copyright(C) 2018 Stamus Networks
+Written by Eric Leblond <eleblond@stamus-networks.com>
+
+This file is part of Scirius.
+
+Scirius is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Scirius is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Scirius.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 import React from 'react';
 import { Notification, NotificationDrawer} from 'patternfly-react';
 import { MenuItem, Icon, Button } from 'patternfly-react';
--- hunt/src/registerServiceWorker.js
+++ hunt/src/registerServiceWorker.js
@@ -1,3 +1,24 @@
+/*
+Copyright(C) 2018 Stamus Networks
+Written by Eric Leblond <eleblond@stamus-networks.com>
+
+This file is part of Scirius.
+
+Scirius is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Scirius is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Scirius.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
