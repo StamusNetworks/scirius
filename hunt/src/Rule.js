526c083576ffc7bc8f6028355879b08f8a6937bb
Eric Leblond <eric@regit.org>
2018-08-22 15:12:35 +0200

    hunt: fix the tick on rule in list

--- hunt/src/Rule.js
+++ hunt/src/Rule.js
@@ -101,7 +101,7 @@ export class RuleInList extends React.Component {
                             localtime: true,
                             min: this.props.from_date,
                             max: Date.now(),
-                            tick: { fit: true, format: '%Y-%m-%d %H:%M'}
+                            tick: { fit: false, format: '%Y-%m-%d %H:%M'}
                      } }
                     }
 		from_date = {this.props.from_date}
