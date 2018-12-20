55f65388a350b1e822042db51ee0cfa065729780
Stoyan Atanasoff <sa@stamus-networks.com>
2018-12-20 18:53:53 +0200

    hunt: moved FilterEditKebab component into separate file

---
+++ hunt/src/FilterEditKebab.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { DropdownKebab, MenuItem } from 'patternfly-react';
+import FilterToggleModal from './FilterToggleModal';
+
+export default class FilterEditKebab extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayToggle = this.displayToggle.bind(this);
+        this.hideToggle = this.hideToggle.bind(this);
+        this.state = { toggle: { show: false, action: 'delete' } };
+        this.closeAction = this.closeAction.bind(this);
+    }
+
+    displayToggle(action) {
+        this.setState({ toggle: { show: true, action } });
+    }
+
+    hideToggle() {
+        this.setState({ toggle: { show: false, action: this.state.toggle.action } });
+    }
+
+    closeAction() {
+        this.setState({ toggle: { show: false, action: 'delete' } });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <DropdownKebab id="filterActions" pullRight>
+                    {this.props.data.index !== 0 && <MenuItem onClick={() => { this.displayToggle('movetop'); }}>
+                        Send Filter to top
+                    </MenuItem>}
+                    <MenuItem onClick={() => { this.displayToggle('move'); }}>
+                        Move Filter
+                    </MenuItem>
+                    <MenuItem onClick={() => { this.displayToggle('movebottom'); }}>
+                        Send Filter to bottom
+                    </MenuItem>
+                    <MenuItem divider />
+                    <MenuItem onClick={() => { this.displayToggle('delete'); }}>
+                        Delete Filter
+                    </MenuItem>
+                </DropdownKebab>
+                <FilterToggleModal show={this.state.toggle.show} action={this.state.toggle.action} data={this.props.data} close={this.closeAction} last_index={this.props.last_index} needUpdate={this.props.needUpdate} />
+            </React.Fragment>
+        );
+    }
+}
+FilterEditKebab.propTypes = {
+    data: PropTypes.any,
+    last_index: PropTypes.any,
+    needUpdate: PropTypes.any,
+};
--- hunt/src/Filters.js
+++ hunt/src/Filters.js
@@ -22,11 +22,11 @@ along with Scirius.  If not, see <http://www.gnu.org/licenses/>.
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import { PAGINATION_VIEW, ListView, ListViewItem, ListViewInfoItem, ListViewIcon, DropdownKebab, MenuItem, Row, Spinner } from 'patternfly-react';
-import FilterToggleModal from './FilterToggleModal';
+import { PAGINATION_VIEW, ListView, ListViewItem, ListViewInfoItem, ListViewIcon, Row, Spinner } from 'patternfly-react';
 import * as config from './config/Api';
 import { HuntList } from './Api';
 import HuntPaginationRow from './HuntPaginationRow';
+import FilterEditKebab from './FilterEditKebab';
 
 export class FiltersList extends HuntList {
     constructor(props) {
@@ -203,55 +203,4 @@ FilterItem.propTypes = {
     rulesets: PropTypes.any,
     needUpdate: PropTypes.any,
     last_index: PropTypes.any,
-};
-
-// eslint-disable-next-line react/no-multi-comp
-class FilterEditKebab extends React.Component {
-    constructor(props) {
-        super(props);
-        this.displayToggle = this.displayToggle.bind(this);
-        this.hideToggle = this.hideToggle.bind(this);
-        this.state = { toggle: { show: false, action: 'delete' } };
-        this.closeAction = this.closeAction.bind(this);
-    }
-
-    displayToggle(action) {
-        this.setState({ toggle: { show: true, action } });
-    }
-
-    hideToggle() {
-        this.setState({ toggle: { show: false, action: this.state.toggle.action } });
-    }
-
-    closeAction() {
-        this.setState({ toggle: { show: false, action: 'delete' } });
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <DropdownKebab id="filterActions" pullRight>
-                    {this.props.data.index !== 0 && <MenuItem onClick={() => { this.displayToggle('movetop'); }}>
-                        Send Filter to top
-                    </MenuItem>}
-                    <MenuItem onClick={() => { this.displayToggle('move'); }}>
-                        Move Filter
-                    </MenuItem>
-                    <MenuItem onClick={() => { this.displayToggle('movebottom'); }}>
-                        Send Filter to bottom
-                    </MenuItem>
-                    <MenuItem divider />
-                    <MenuItem onClick={() => { this.displayToggle('delete'); }}>
-                        Delete Filter
-                    </MenuItem>
-                </DropdownKebab>
-                <FilterToggleModal show={this.state.toggle.show} action={this.state.toggle.action} data={this.props.data} close={this.closeAction} last_index={this.props.last_index} needUpdate={this.props.needUpdate} />
-            </React.Fragment>
-        );
-    }
-}
-FilterEditKebab.propTypes = {
-    data: PropTypes.any,
-    last_index: PropTypes.any,
-    needUpdate: PropTypes.any,
 };
