7095a43ec5ea2216dd93694174706ff07a5decd9
Stoyan Atanasoff <sa@stamus-networks.com>
2019-04-04 22:58:23 +0300

    hunt: packages dependencies upgrade

--- hunt/config/webpack.config.prod.js
+++ hunt/config/webpack.config.prod.js
@@ -186,7 +186,6 @@ module.exports = {
                                             loader: require.resolve('css-loader'),
                                             options: {
                                                 importLoaders: 1,
-                                                minimize: true,
                                                 sourceMap: shouldUseSourceMap,
                                             },
                                         },
