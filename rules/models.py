StamusNetworks/scirius

commit 9aec8e3eed0d2916a630c4fbba4d04a5b4360f43

rules: fix cache that prevents to be deleted correctly on exceptions

rules/models.py
@@ -2157,14 +2157,12 @@ class Rule(models.Model, Transformable, Cache):
         return
 
     def test(self, ruleset):
-        self.enable_cache()
         try:
+            self.enable_cache()
             test = ruleset.test_rule_buffer(self.generate_content(ruleset), single = True)
+        finally:
             self.disable_cache()
-            return test
-        except:
-            self.disable_cache()
-        return False
+        return test
 
     def toggle_availability(self):
         self.category.source.needs_test()
@@ -2748,16 +2746,17 @@ class Ruleset(models.Model, Transformable):
         file_content = "# Rules file for %s generated by Scirius at %s\n" % (self.name, unicode(timezone.now()))
 
         if len(rules) > 0:
-            Rule.enable_cache()
+            try:
+                Rule.enable_cache()
 
-            rules_content = []
-            for rule in rules:
-                c = rule.generate_content(self)
-                if c:
-                    rules_content.append(c)
-            file_content += "\n".join(rules_content)
-
-            Rule.disable_cache()
+                rules_content = []
+                for rule in rules:
+                    c = rule.generate_content(self)
+                    if c:
+                        rules_content.append(c)
+                file_content += "\n".join(rules_content)
+            finally:
+                Rule.disable_cache()
 
         return file_content
 

rules/rest_api.py
@@ -732,64 +732,66 @@ class RuleViewSet(SciriusReadOnlyModelViewSet):
                 raise serializers.ValidationError({'filters': ['Wrong filter value "%s" for key "%s".' % (value_str, key_str)]})
 
         res = {}
-        Rule.enable_cache()
+        try:
+            Rule.enable_cache()
 
-        for ruleset in Ruleset.objects.all():
-            trans_rules = RuleTransformation.objects.filter(ruleset=ruleset, **params)
-            trans_cats = CategoryTransformation.objects.filter(ruleset=ruleset, **params)
-            trans_rulesets = RulesetTransformation.objects.filter(ruleset_transformation=ruleset, **params)
+            for ruleset in Ruleset.objects.all():
+                trans_rules = RuleTransformation.objects.filter(ruleset=ruleset, **params)
+                trans_cats = CategoryTransformation.objects.filter(ruleset=ruleset, **params)
+                trans_rulesets = RulesetTransformation.objects.filter(ruleset_transformation=ruleset, **params)
 
-            all_rules = set()
-            key = Transformation.Type(key_str)
-            value = None
+                all_rules = set()
+                key = Transformation.Type(key_str)
+                value = None
 
-            if key == Transformation.ACTION:
-                value = Transformation.ActionTransfoType(value_str)
-            elif key == Transformation.LATERAL:
-                value = Transformation.LateralTransfoType(value_str)
-            elif key == Transformation.TARGET:
-                value = Transformation.TargetTransfoType(value_str)
+                if key == Transformation.ACTION:
+                    value = Transformation.ActionTransfoType(value_str)
+                elif key == Transformation.LATERAL:
+                    value = Transformation.LateralTransfoType(value_str)
+                elif key == Transformation.TARGET:
+                    value = Transformation.TargetTransfoType(value_str)
 
-            if ruleset.pk not in res:
-                res[ruleset.pk] = {'name': ruleset.name,
-                                   'transformation': {'transfo_key': key_str, 'transfo_value': value_str},
-                                   'rules': []
-                                   }
+                if ruleset.pk not in res:
+                    res[ruleset.pk] = {'name': ruleset.name,
+                                       'transformation': {'transfo_key': key_str, 'transfo_value': value_str},
+                                       'rules': []
+                                       }
 
-            for trans in trans_rules:
-                all_rules.add(trans.rule_transformation.pk)
+                for trans in trans_rules:
+                    all_rules.add(trans.rule_transformation.pk)
 
-            for trans in trans_cats:
-                category = trans.category_transformation
-                for rule in category.rule_set.all():
-                    rule_trans_value = rule.get_transformation(ruleset, key=key)
-                    if rule_trans_value is None or rule_trans_value == value:
-                        all_rules.add(rule.sid)
+                for trans in trans_cats:
+                    category = trans.category_transformation
+                    for rule in category.rule_set.all():
+                        rule_trans_value = rule.get_transformation(ruleset, key=key)
+                        if rule_trans_value is None or rule_trans_value == value:
+                            all_rules.add(rule.sid)
 
-            if trans_rulesets:
-                for category in ruleset.categories.all():
-                    trans_cat = CategoryTransformation.objects.filter(ruleset=ruleset, category_transformation=category)
+                if trans_rulesets:
+                    for category in ruleset.categories.all():
+                        trans_cat = CategoryTransformation.objects.filter(ruleset=ruleset, category_transformation=category)
 
-                    if len(trans_cat) == 0:
-                        for rule in category.rule_set.all():
-                            rule_trans_value = rule.get_transformation(ruleset, key=key)
-                            if rule_trans_value is None or rule_trans_value == value:
-                                all_rules.add(rule.sid)
-                    else:
-                        for trans in trans_cat:
+                        if len(trans_cat) == 0:
                             for rule in category.rule_set.all():
                                 rule_trans_value = rule.get_transformation(ruleset, key=key)
-                                if trans.key == key and trans.value == value:
-                                    if rule_trans_value is None or rule_trans_value == value:
-                                        all_rules.add(rule.sid)
-                                else:
-                                    if rule_trans_value == value:
-                                        all_rules.add(rule.sid)
+                                if rule_trans_value is None or rule_trans_value == value:
+                                    all_rules.add(rule.sid)
+                        else:
+                            for trans in trans_cat:
+                                for rule in category.rule_set.all():
+                                    rule_trans_value = rule.get_transformation(ruleset, key=key)
+                                    if trans.key == key and trans.value == value:
+                                        if rule_trans_value is None or rule_trans_value == value:
+                                            all_rules.add(rule.sid)
+                                    else:
+                                        if rule_trans_value == value:
+                                            all_rules.add(rule.sid)
 
-            res[ruleset.pk]['rules'] = list(all_rules)
-            res[ruleset.pk]['rules_count'] = len(all_rules)
+                res[ruleset.pk]['rules'] = list(all_rules)
+                res[ruleset.pk]['rules_count'] = len(all_rules)
+        finally:
+            Rule.disable_cache()
 
-        Rule.disable_cache()
         return Response(res)
 
     @detail_route(methods=['get'])

commit eda2f1248404708980e00c9bea5700156e1d55fb

rules: bring back previous behaviour on Source.test
Broken in 9aec8e3eed0d2916a630c4fbba4d04a5b4360f43

rules/models.py
@@ -2305,6 +2305,8 @@ class Rule(models.Model, Transformable, Cache):
         try:
             self.enable_cache()
             test = ruleset.test_rule_buffer(self.generate_content(ruleset), single = True)
+        except:
+            return False
         finally:
             self.disable_cache()
         return test