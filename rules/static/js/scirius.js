548f39e740f580b5e35a67e45393129e83a40de3
Erling Rørvik <potrik98@gmail.com>
2019-08-01 15:08:39 +0200

    rules: reformat draw_sunburst because it's unreadable

--- rules/static/js/scirius.js
+++ rules/static/js/scirius.js
@@ -422,7 +422,8 @@ function build_path(d) {
 }
 window.build_path = build_path;
 
-function draw_sunburst(from_date, hosts, filter, callback, sort_order, category=undefined, draw_timeline_function=undefined) {
+function draw_sunburst(from_date, hosts, filter, callback, sort_order,
+                       category = undefined, draw_timeline_function = undefined) {
     /*
      * Draws the sunburst graph on the suricata/index page.
      * from_date: from date in epoch ms
@@ -437,174 +438,171 @@ function draw_sunburst(from_date, hosts, filter, callback, sort_order, category=
      *    update the timeline.
      */
 
-        var esurl = "/rest/rules/es/rules_per_category/?from_date=" + from_date + "&hosts=" + hosts.join()
-        if (filter) {
-            esurl = esurl + "&qfilter=" + filter;
-        }
-        $.ajax(
-         {
-         type:"GET",
-         url:esurl,
-         success: function(data) {
-         if (!data) {
-              $("#circles").append("No data to build the graph");
-              return;
-         }
-var height = 300;
-var width = 300;
-var radius = Math.min(width, height) / 2;
-
-var x = d3.scale.linear()
-    .range([0, 2 * Math.PI]);
-
-var y = d3.scale.sqrt()
-    .range([0, radius]);
-
-var color = d3.scale.category20b();
-
-d3.select("#circles > *").remove();
-var svg = d3.select("#circles").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
-
-var partition = d3.layout.partition()
-    .sort(null)
-    .value(function(d) { return d.doc_count; })
-    .children(function(d) { return d.children; })
-
-var arc = d3.svg.arc()
-    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
-    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
-    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
-    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
-
-  // Keep track of the node that is currently being displayed as the root.
-  var node;
-  var data_root = data;
-  node = data_root;
-  var path = svg.datum(data_root).selectAll("path")
-      .data(partition.nodes)
-    .enter().append("path")
-      .attr("d", arc)
-      //.style("fill", function(d) { return color(d.value); })
-      .style("fill", function(d) { return color(d.key); })
-      .on("click", click)
-      .each(stash);
-
-
-
-  $('path').mouseover(function(){
-      var d = this.__data__;
-      var tooltip = build_path(d);
-      $( "#circles").append("<div id='circles_tooltip'>" + tooltip + "</div>");
-  });
-  $('path').mouseout(function(){
-      var d = this.__data__;
-      $( "#circles_tooltip").remove();
-  });
-
-  d3.selectAll("input").on("change", function change() {
-    var value = this.value === "count"
-        ? function() { return 1; }
-        : function(d) { return d.size; };
-
-    path
-        .data(partition.value(value).nodes)
-      .transition()
-        .duration(1000)
-        .attrTween("d", arcTweenData);
-  });
-
-  function click(d) {
-    node = d;
-    if (d.children == undefined) {
-         window.open("/rules/rule/pk/" + d.key,"_self");
+    var esurl = "/rest/rules/es/rules_per_category/?from_date=" + from_date + "&hosts=" + hosts.join()
+    if (filter) {
+        esurl = esurl + "&qfilter=" + filter;
     }
-    var tooltip = build_path(d);
-    $("#filter").empty();
-    if (tooltip.length) {
-        $("#filter").append("Filter: " + tooltip);
-    }
-    if (d.key == "categories") {
-        if (draw_timeline_function !== undefined) {
-            draw_timeline_function(null, d);
-        } else {
-            draw_timeline(from_date, hosts, null, null);
-        }
-        load_rules(from_date, hosts, null, null, sort_order);
-    } else {
-        if (draw_timeline_function !== undefined) {
-            draw_timeline_function('alert.category.raw:"'+d.key+'"', d);
-        } else {
-            draw_timeline(from_date, hosts, 'alert.category.raw:"'+d.key+'"', null);
-        }
-        load_rules(from_date, hosts, 'alert.category.raw:"'+d.key+'"', null, sort_order);
-    }
-    path.transition()
-      .duration(1000)
-      .attrTween("d", arcTweenZoom(d));
-  }
+    $.ajax({
+            type: "GET",
+            url: esurl,
+            success: function (data) {
+                if (!data) {
+                    $("#circles").append("No data to build the graph");
+                    return;
+                }
+                var height = 300;
+                var width = 300;
+                var radius = Math.min(width, height) / 2;
 
-  if (category !== undefined) {
-      for (var c in path.data()) {
-        c = path.data()[c];
-        if (c.key == category.key) {
-          category = c;
-          break;
-        }
-      }
-      path.transition()
-          .duration(500)
-          .attrTween("d", arcTweenZoom(category));
-  }
+                var x = d3.scale.linear()
+                    .range([0, 2 * Math.PI]);
 
-d3.select(self.frameElement).style("height", height + "px");
+                var y = d3.scale.sqrt()
+                    .range([0, radius]);
 
-// Setup for switching data: stash the old values for transition.
-function stash(d) {
-  d.x0 = d.x;
-  d.dx0 = d.dx;
-}
+                var color = d3.scale.category20b();
 
-// When switching data: interpolate the arcs in data space.
-function arcTweenData(a, i) {
-  var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-  function tween(t) {
-    var b = oi(t);
-    a.x0 = b.x;
-    a.dx0 = b.dx;
-    return arc(b);
-  }
-  if (i == 0) {
-   // If we are on the first arc, adjust the x domain to match the root node
-   // at the current zoom level. (We only need to do this once.)
-    var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
-    return function(t) {
-      x.domain(xd(t));
-      return tween(t);
-    };
-  } else {
-    return tween;
-  }
-}
+                d3.select("#circles > *").remove();
+                var svg = d3.select("#circles").append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .append("g")
+                    .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
 
-// When zooming: interpolate the scales.
-function arcTweenZoom(d) {
-  var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
-      yd = d3.interpolate(y.domain(), [d.y, 1]),
-      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
-  return function(d, i) {
-    return i
-        ? function(t) { return arc(d); }
-        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
-  };
-}
+                var partition = d3.layout.partition()
+                    .sort(null)
+                    .value(function (d) { return d.doc_count; })
+                    .children(function (d) { return d.children; })
 
-     callback();
-        },
-        });
+                var arc = d3.svg.arc()
+                    .startAngle(function (d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+                    .endAngle(function (d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+                    .innerRadius(function (d) { return Math.max(0, y(d.y)); })
+                    .outerRadius(function (d) { return Math.max(0, y(d.y + d.dy)); });
+
+                // Keep track of the node that is currently being displayed as the root.
+                var node;
+                var data_root = data;
+                node = data_root;
+                var path = svg.datum(data_root).selectAll("path")
+                    .data(partition.nodes)
+                    .enter().append("path")
+                    .attr("d", arc)
+                    //.style("fill", function(d) { return color(d.value); })
+                    .style("fill", function (d) { return color(d.key); })
+                    .on("click", click)
+                    .each(stash);
+
+                $('path').mouseover(function () {
+                    var d = this.__data__;
+                    var tooltip = build_path(d);
+                    $("#circles").append("<div id='circles_tooltip'>" + tooltip + "</div>");
+                });
+                $('path').mouseout(function () {
+                    var d = this.__data__;
+                    $("#circles_tooltip").remove();
+                });
+
+                d3.selectAll("input").on("change", function change() {
+                    var value = this.value === "count"
+                        ? function () { return 1; }
+                        : function (d) { return d.size; };
+
+                    path
+                        .data(partition.value(value).nodes)
+                        .transition()
+                        .duration(1000)
+                        .attrTween("d", arcTweenData);
+                });
+
+                function click(d) {
+                    node = d;
+                    if (d.children == undefined) {
+                        window.open("/rules/rule/pk/" + d.key, "_self");
+                    }
+                    var tooltip = build_path(d);
+                    $("#filter").empty();
+                    if (tooltip.length) {
+                        $("#filter").append("Filter: " + tooltip);
+                    }
+                    if (d.key == "categories") {
+                        if (draw_timeline_function !== undefined) {
+                            draw_timeline_function(null, d);
+                        } else {
+                            draw_timeline(from_date, hosts, null, null);
+                        }
+                        load_rules(from_date, hosts, null, null, sort_order);
+                    } else {
+                        if (draw_timeline_function !== undefined) {
+                            draw_timeline_function('alert.category.raw:"' + d.key + '"', d);
+                        } else {
+                            draw_timeline(from_date, hosts, 'alert.category.raw:"' + d.key + '"', null);
+                        }
+                        load_rules(from_date, hosts, 'alert.category.raw:"' + d.key + '"', null, sort_order);
+                    }
+                    path.transition()
+                        .duration(1000)
+                        .attrTween("d", arcTweenZoom(d));
+                }
+
+                if (category !== undefined) {
+                    for (var c in path.data()) {
+                        c = path.data()[c];
+                        if (c.key == category.key) {
+                            category = c;
+                            break;
+                        }
+                    }
+                    path.transition()
+                        .duration(500)
+                        .attrTween("d", arcTweenZoom(category));
+                }
+
+                d3.select(self.frameElement).style("height", height + "px");
+
+                // Setup for switching data: stash the old values for transition.
+                function stash(d) {
+                    d.x0 = d.x;
+                    d.dx0 = d.dx;
+                }
+
+                // When switching data: interpolate the arcs in data space.
+                function arcTweenData(a, i) {
+                    var oi = d3.interpolate({ x: a.x0, dx: a.dx0 }, a);
+                    function tween(t) {
+                        var b = oi(t);
+                        a.x0 = b.x;
+                        a.dx0 = b.dx;
+                        return arc(b);
+                    }
+                    if (i == 0) {
+                        // If we are on the first arc, adjust the x domain to match the root node
+                        // at the current zoom level. (We only need to do this once.)
+                        var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
+                        return function (t) {
+                            x.domain(xd(t));
+                            return tween(t);
+                        };
+                    } else {
+                        return tween;
+                    }
+                }
+
+                // When zooming: interpolate the scales.
+                function arcTweenZoom(d) {
+                    var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+                        yd = d3.interpolate(y.domain(), [d.y, 1]),
+                        yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+                    return function (d, i) {
+                        return i
+                            ? function (t) { return arc(d); }
+                            : function (t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+                    };
+                }
+
+                callback();
+            }
+    });
 }
 window.draw_sunburst = draw_sunburst;
 
