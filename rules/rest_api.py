358d8d57f040ae5ac3a254cd0ee4babe66ca46e4
Nicolas Frisoni <nfrisoni@stamus-networks.com>
2020-04-27 16:49:10 +0200

    rules: add version field on sources rest api

--- rules/rest_api.py
+++ rules/rest_api.py
@@ -1374,7 +1374,7 @@ class BaseSourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Source
         fields = ('pk', 'name', 'created_date', 'updated_date', 'method', 'datatype', 'uri', 'cert_verif',
-                  'cats_count', 'rules_count', 'use_iprep')
+                  'cats_count', 'rules_count', 'use_iprep', 'version')
         read_only_fields = ('pk', 'created_date', 'updated_date', 'method', 'datatype', 'cert_verif',
                             'cats_count', 'rules_count',)
 
