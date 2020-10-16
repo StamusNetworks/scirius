e3cdbfd525cecd5cd25dfd7c410a673fbd040b9b
Stoyan Atanasoff <sa@stamus-networks.com>
2020-10-16 11:51:40 +0300

    scirius: moved all packages to package.json

--- npm/scirius-bundle.js
+++ npm/scirius-bundle.js
@@ -1,12 +1,24 @@
+window.$ = window.jQuery = require('jquery');
+window.c3 = require("c3");
+window.SparkMD5 = require("spark-md5");
+$.fn.datetimepicker = require('eonasdan-bootstrap-datetimepicker');
+
 // CSS
-import "../rules/static/rules/bootstrap.min.css";
-import "../rules/static/rules/nv.d3.min.css";
-import "../rules/static/rules/c3.min.css";
-import "../rules/static/rules/static.css";
-import "../node_modules/patternfly/dist/css/patternfly.css";
-import "../node_modules/patternfly/dist/css/patternfly-additions.css";
-import "../scss/_styles.scss";
+require("bootstrap/dist/css/bootstrap.min.css");
+require("nvd3/build/nv.d3.min.css");
+require("c3/c3.min.css");
+require("../rules/static/rules/static.css");
+require("patternfly/dist/css/patternfly.css");
+require("patternfly/dist/css/patternfly-additions.css");
+require("../scss/_styles.scss");
+
 // JS
-import "../rules/static/js/bootstrap.min.js";
-import "../rules/static/js/scirius.js";
-import "../node_modules/patternfly/dist/js/patternfly.js";
+require("../node_modules/patternfly/dist/js/patternfly.js");
+require("jquery-treeview");
+require("bootstrap");
+require("jquery-ui");
+require("jquery-knob");
+require("d3");
+require("nvd3");
+require("../node_modules/blueimp-file-upload/js/jquery.fileupload.js");
+require("../rules/static/js/scirius.js");
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,90 @@
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-const extractSass = new ExtractTextPlugin({
-  filename: "styles.css",
-  disable: process.env.NODE_ENV === "development"
-});
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./npm/scirius-bundle.js",
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['eslint-loader']
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: extractSass.extract({
-          use: [
+    entry: "./npm/scirius-bundle.js",
+    module: {
+        rules: [
             {
-              loader: "css-loader"
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
             },
             {
-              loader: "sass-loader"
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['eslint-loader']
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        // After all CSS loaders we use plugin to do his work.
+                        // It gets all transformed CSS and extracts it into separate
+                        // single bundled file
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                    /* ... Other loaders ... */
+                ]
+            },
+            {
+                // Now we apply rule for images
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        // Using file-loader for these files
+                        loader: "file-loader",
+
+                        // In options we can set different things like format
+                        // and directory to save
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                // Apply rule for fonts files
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                    {
+                        // Using file-loader too
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                loader: "svg-inline-loader"
             }
-          ],
-          fallback: "style-loader"
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
         })
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif|png)$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.svg$/,
-        loader: "svg-inline-loader"
-      }
-    ]
-  },
-  plugins: [extractSass],
-  output: {
-    path: path.resolve(__dirname, "rules/static/dist"),
-    filename: "scirius-bundle.js"
-  }
+    ],
+    output: {
+        path: path.resolve(__dirname, "rules/static/dist"),
+        filename: "scirius-bundle.js"
+    }
 };
