6271a90afeea8fed4c38be578ac446f10db1106a
Tzvetan Marinov <tmarinov@stamus-networks.com>
2020-11-30 14:12:54 +0200

    hunt: filter icons are overlapping with text on longer filters

--- hunt/src/common/components/FilterItem/FilterItem.js
+++ hunt/src/common/components/FilterItem/FilterItem.js
@@ -8,30 +8,32 @@ const FilterItem = (props) => {
     const displayValue = (props.filter.label) ? props.filter.label : `${props.filter.id}:${props.filter.value}`;
     return <li>
         <span className={`hunt-filter label label-info ${negated}`}>
+            <div>
+                {props.filterType !== sections.HISTORY && <a
+                    href="#"
+                    className="pf-edit-button filter-action edit"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        props.onEdit()
+                    }}
+                >
+                    <span className="pficon pficon-edit" aria-hidden="true" />
+                    <span className="sr-only">Edit</span>
+                </a>}
+                { props.children }
+                <a
+                    href="#"
+                    className="pf-remove-button filter-action delete"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        props.onRemove();
+                    }}
+                >
+                    <span className="pficon pficon-close" aria-hidden="true" />
+                    <span className="sr-only">Remove</span>
+                </a>
+            </div>
             <div className={'label-content'}>{displayValue}</div>
-            {props.filterType !== sections.HISTORY && <a
-                href="#"
-                className="pf-edit-button filter-action edit"
-                onClick={(e) => {
-                    e.preventDefault();
-                    props.onEdit()
-                }}
-            >
-                <span className="pficon pficon-edit" aria-hidden="true" />
-                <span className="sr-only">Edit</span>
-            </a>}
-            { props.children }
-            <a
-                href="#"
-                className="pf-remove-button filter-action delete"
-                onClick={(e) => {
-                    e.preventDefault();
-                    props.onRemove();
-                }}
-            >
-                <span className="pficon pficon-close" aria-hidden="true" />
-                <span className="sr-only">Remove</span>
-            </a>
         </span>
     </li>
 }
