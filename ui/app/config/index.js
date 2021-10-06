b1ab9490953a6c8303e28ff95f11cefad634397e
Tzvetan Marinov <tmarinov@stamus-networks.com>
2021-10-06 16:38:34 +0300

    ui: config update

--- ui/app/config/index.js
+++ ui/app/config/index.js
@@ -1,6 +1,6 @@
 export const API_URL = '/rest/appliances';
 export const RULES_URL = '/rest/rules';
-export const APP_URL = '/appliances/str';
+export const APP_URL = '/stamus';
 export const HUNT_URL = '/rules/hunt';
 export const SYSTEM_SETTINGS_PATH = '/rest/rules/system_settings';
 export const RULE_PATH = '/rest/rules/rule';
