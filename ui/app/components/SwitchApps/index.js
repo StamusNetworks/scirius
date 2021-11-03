88cf51884eeb8bbd94d1e9885d330b852482a25b
Tzvetan Marinov <tmarinov@stamus-networks.com>
2021-11-03 09:23:57 +0200

    ui: fixed broken AppSwitcher

--- ui/app/components/SwitchApps/index.js
+++ ui/app/components/SwitchApps/index.js
@@ -1,15 +1,17 @@
- import React, { useState, useEffect } from 'react';
- import { connect } from 'react-redux';
- import PropTypes from 'prop-types';
- import styled from 'styled-components';
- import { Spin } from 'antd';
- import { LoadingOutlined } from '@ant-design/icons';
- import LaunchRounded from "@material-ui/icons/LaunchRounded";
- 
- import * as config from 'ui/config';
- import request from 'utils/request';
- 
- const AppsContainer = styled.div`
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createStructuredSelector } from 'reselect';
+import styled from 'styled-components';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import LaunchRounded from "@material-ui/icons/LaunchRounded";
+
+import * as config from 'ui/config';
+import request from 'utils/request';
+import selectors from 'ui/containers/App/selectors';
+
+const AppsContainer = styled.div`
    width: 100px;
    height: 96px;
    display: flex;
@@ -22,8 +24,8 @@
      justify-content: center;
    }
  `;
- 
- const AppItem = styled.a`
+
+const AppItem = styled.a`
    height: 32px;
    display: flex;
    justify-content: space-between;
@@ -40,59 +42,60 @@
      height: 14px;
    }
  `;
- 
- const IconLabel = styled.div`
+
+const IconLabel = styled.div`
    color: #005792;
  `;
- 
- function SwitchApps({ user }) {
-   const {
-     data: { permissions=[] },
-     request: { loading },
-   } = user;
-   const [systemSettings, setSystemSettings] = useState({});
 
-   useEffect(() => {
-     (async () => setSystemSettings(await request(config.SYSTEM_SETTINGS_PATH)))();
-   }, []);
- 
-   return (
-     <React.Fragment>
-       <AppsContainer>
-         {systemSettings.kibana && permissions.includes('rules.events_kibana') && (
-           <AppItem href={systemSettings.kibana_url} target="_blank">
-             <IconLabel>Kibana</IconLabel>
-             <LaunchRounded style={{color: "currentColor", strokeWidth: 1.5, fill: "#005792"}} />
-           </AppItem>
-         )}
-         {systemSettings.evebox && permissions.includes('rules.events_evebox') && (
-           <AppItem href={systemSettings.evebox_url} target="_blank">
-             <IconLabel>Eventbox</IconLabel>
-             <LaunchRounded style={{color: "currentColor", strokeWidth: 1.5, fill: "#005792"}} />
-           </AppItem>
-         )}
-         {systemSettings.cyberchef && (
-           <AppItem href={systemSettings.cyberchef_url} target="_blank">
-             <IconLabel>Cyberchef</IconLabel>
-             <LaunchRounded style={{color: "currentColor", strokeWidth: 1.5, fill: "#005792"}} />
-           </AppItem>
-         )}
-         {loading && <Spin indicator={<LoadingOutlined spin />} />}
-       </AppsContainer>
-     </React.Fragment>
-   );
- }
- 
- SwitchApps.propTypes = {
-   user: PropTypes.shape({
-     data: PropTypes.object,
-     request: PropTypes.object,
-   }).isRequired,
- };
- 
- const mapStateToProps = ({ global: { user } }) => ({ user });
- 
- export default connect(
-   mapStateToProps
- )(SwitchApps);
- 
+function SwitchApps({ user }) {
+  const {
+    data: { permissions = [] },
+    request: { loading },
+  } = user;
+  const [systemSettings, setSystemSettings] = useState({});
+
+  useEffect(() => {
+    (async () => setSystemSettings(await request(config.SYSTEM_SETTINGS_PATH)))();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <AppsContainer>
+        {systemSettings.kibana && permissions.includes('rules.events_kibana') && (
+          <AppItem href={systemSettings.kibana_url} target="_blank">
+            <IconLabel>Kibana</IconLabel>
+            <LaunchRounded style={{ color: "currentColor", strokeWidth: 1.5, fill: "#005792" }} />
+          </AppItem>
+        )}
+        {systemSettings.evebox && permissions.includes('rules.events_evebox') && (
+          <AppItem href={systemSettings.evebox_url} target="_blank">
+            <IconLabel>Eventbox</IconLabel>
+            <LaunchRounded style={{ color: "currentColor", strokeWidth: 1.5, fill: "#005792" }} />
+          </AppItem>
+        )}
+        {systemSettings.cyberchef && (
+          <AppItem href={systemSettings.cyberchef_url} target="_blank">
+            <IconLabel>Cyberchef</IconLabel>
+            <LaunchRounded style={{ color: "currentColor", strokeWidth: 1.5, fill: "#005792" }} />
+          </AppItem>
+        )}
+        {loading && <Spin indicator={<LoadingOutlined spin />} />}
+      </AppsContainer>
+    </React.Fragment>
+  );
+}
+
+SwitchApps.propTypes = {
+  user: PropTypes.shape({
+    data: PropTypes.object,
+    request: PropTypes.object,
+  }).isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  user: selectors.makeSelectUser(),
+});
+
+export default connect(
+  mapStateToProps
+)(SwitchApps);
