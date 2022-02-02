6da520f68a682a4a620b8a76963408add1c0b059
Laurent Defert <lds@stamus-networks.com>
2022-02-02 11:59:41 -0500

    ui: remove spaces from CyberChef and EveBox menu entries

--- ui/app/pages/CyberChef/CyberChef.js
+++ ui/app/pages/CyberChef/CyberChef.js
@@ -4,6 +4,7 @@ CyberChef.metadata = {
   category: 'OTHER_APPS',
   url: (systemSettings) => systemSettings.cyberchef_url,
   access: (permissions, systemSettings) => systemSettings.cyberchef,
+  title: 'CyberChef',
 }
 
 export default CyberChef;
--- ui/app/pages/EveBox/EveBox.js
+++ ui/app/pages/EveBox/EveBox.js
@@ -4,6 +4,7 @@ EveBox.metadata = {
   category: 'OTHER_APPS',
   url: (systemSettings) => systemSettings.evebox_url,
   access: (permissions, systemSettings) => systemSettings.evebox && permissions.includes('rules.events_evebox'),
+  title: 'EveBox',
 }
 
 export default EveBox;
