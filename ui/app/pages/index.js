c1076139a49fe1eeb34ce5a291f29a3ace141303
Laurent Defert <lds@stamus-networks.com>
2022-03-21 04:26:37 -0400

    ui: remove Enriched Hunting tab

--- ui/app/pages/index.js
+++ ui/app/pages/index.js
@@ -1,5 +1,4 @@
 import NotFoundPage from './NotFound/NotFound';
-import EnrichedHunting from './EnrichedHunting/EnrichedHunting';
 import { Appliances, Users, Sources, Monitoring, Ryod, Other } from './Administration/Administration';
 import Kibana from './Kibana/Kibana';
 import EveBox from './EveBox/EveBox';
@@ -7,7 +6,6 @@ import CyberChef from './CyberChef/CyberChef';
 
 export default {
   NotFoundPage,
-  EnrichedHunting,
   Appliances,
   Users,
   Sources,
