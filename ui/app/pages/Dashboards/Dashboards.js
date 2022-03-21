0fe1ba3629179759d0657b7e86ce9215f4b8caa7
Laurent Defert <lds@stamus-networks.com>
2022-03-21 11:11:59 -0400

    ui: Dashboard page

--- ui/app/pages/Dashboards/Dashboards.js
+++ ui/app/pages/Dashboards/Dashboards.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { PAGE_STATE } from 'constants';
 import UIBreadcrumb from 'ui/components/UIBreadcrumb';
 import { Link } from 'ui/helpers/Link';
+import HuntApp from 'ui/containers/HuntApp';
 
 const Dashboards = () => (
   <div>
@@ -11,6 +13,7 @@ const Dashboards = () => (
       ]}
     />
     Dashboards
+    <HuntApp page={PAGE_STATE.dashboards} />
   </div>
 );
 Dashboards.metadata = {
