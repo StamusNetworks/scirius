d06a9da6709a1a355eb747b2eb914dcd699f85be
Laurent Defert <lds@stamus-networks.com>
2022-03-21 12:17:45 -0400

    ui: fromDate / toDate selectors for Hunt

--- ui/app/containers/HuntApp/stores/filterParams.js
+++ ui/app/containers/HuntApp/stores/filterParams.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import { default as globalSelectors } from 'ui/containers/App/selectors';
 import { createSelector } from 'reselect';
 
 export const FILTER_PARAMS_SET = 'Hunt/HuntApp/FILTER_PARAM_SET';
@@ -89,4 +90,10 @@ export const reducer = (state = initialState, action) => {
 export const selectFilterParamsStore = (state) => state.filterParams || {};
 export const makeSelectFilterParam = (paramName) => createSelector(selectFilterParamsStore, (globalState) => globalState[paramName]);
 export const makeSelectFilterAbsolute = () => createSelector(selectFilterParamsStore, (globalState) => globalState.absolute);
-export const makeSelectFilterParams = () => createSelector(selectFilterParamsStore, (globalState) => globalState);
+export const makeSelectFilterParams = () => createSelector([globalSelectors.makeSelectStartDate(), globalSelectors.makeSelectEndDate()], (startDate, endDate) => {
+  return {
+    fromDate: startDate.unix() * 1000.0,
+    toDate: endDate.unix() * 1000.0,
+    duration: 0
+  };
+});
