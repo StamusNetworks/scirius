c20185c64293092db48571a521f08dbf7a947c5b
Stoyan Atanasoff <sa@stamus-networks.com>
2022-03-29 16:37:57 +0300

    hunt: removed hunt from gitlab ci

--- hunt/config/webpack.config.eslint.js
+++
@@ -1,7 +0,0 @@
-// set NODE_ENV, then load webpack.config.dev to give eslint access to aliases
-// defined in webpack config (see module.exports.resolve.alias in webpack.config.dev.js)
-process.env.NODE_ENV = 'dev';
-
-const webpackDev = require('./webpack.config.dev');
-
-module.exports = webpackDev;
