ecf6ddf64402bc0b193e89571e4f084e08f4bba9
Stoyan Atanasoff <sa@stamus-networks.com>
2022-07-20 22:48:13 +0300

    ui: created buildListParams helper function

---
+++ ui/app/helpers/buildListParams.js
@@ -0,0 +1,36 @@
+/*
+ * this function does sanity checks for the object retrieved from local localStorage
+ */
+const buildListParams = (params, def) => {
+  params = params ?? {};
+
+  if (!params?.pagination) {
+    params.pagination = {};
+  }
+  if (!params?.pagination?.page) {
+    params.pagination.page = def?.pagination?.page || 1;
+  }
+  if (!params?.pagination?.perPage) {
+    params.pagination.perPage = def?.pagination?.perPage || 6;
+  }
+  if (!params?.pagination?.perPageOptions) {
+    params.pagination.perPageOptions = def?.pagination?.perPageOptions || [6, 10, 15, 25];
+  }
+  /* eslint-disable camelcase */
+  if (!params?.view_type) {
+    /* eslint-disable camelcase */
+    params.view_type = def?.view_type || 'list';
+  }
+  if (!params?.sort) {
+    params.sort = def?.sort || {};
+  }
+  if (!params?.sort?.id) {
+    params.sort.id = def?.sort?.id || 'created';
+  }
+  if (!params?.sort?.id) {
+    params.sort.asc = def?.params?.sort?.id || false;
+  }
+  return params;
+};
+
+export default buildListParams;
