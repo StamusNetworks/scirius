da15b8e9919fe50b654ba76f34af1b0771059bc2
Stoyan Atanasoff <sa@stamus-networks.com>
2022-07-28 16:43:02 +0300

    ui: fixed permissions for administration pages

--- ui/app/pages/Administration/Administration.js
+++ ui/app/pages/Administration/Administration.js
@@ -8,21 +8,29 @@ export const Other = () => null;
 Appliances.metadata = {
   category: 'ADMINISTRATION',
   url: () => '/appliances',
+  // eslint-disable-next-line camelcase
+  access: permissions => !!permissions?.rules?.configuration_view,
 };
 
 Users.metadata = {
   category: 'ADMINISTRATION',
   url: () => '/accounts',
+  // eslint-disable-next-line camelcase
+  access: permissions => !!permissions?.rules?.configuration_auth,
 };
 
 Sources.metadata = {
   category: 'ADMINISTRATION',
   url: () => '/rules/source',
+  // eslint-disable-next-line camelcase
+  access: permissions => !!permissions?.rules?.source_view,
 };
 
 Monitoring.metadata = {
   category: 'ADMINISTRATION',
   url: () => '/appliances/monitoring',
+  // eslint-disable-next-line camelcase
+  access: permissions => !!permissions?.rules?.configuration_view,
 };
 
 Ryod.metadata = {
