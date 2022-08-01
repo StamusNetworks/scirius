0903d952761e8a2592f93b67c7366b4789283af1
Stoyan Atanasoff <sa@stamus-networks.com>
2022-08-01 20:07:15 +0300

    ui: reverted missing selectors

--- ui/app/containers/HuntApp/stores/filterParams.js
+++ ui/app/containers/HuntApp/stores/filterParams.js
@@ -1,6 +1,9 @@
 import { default as globalSelectors } from 'ui/containers/App/selectors';
 import { createSelector } from 'reselect';
 
+export const selectFilterParamsStore = state => state.filterParams || {};
+export const makeSelectFilterParam = paramName => createSelector(selectFilterParamsStore, globalState => globalState[paramName]);
+export const makeSelectFilterAbsolute = () => createSelector(selectFilterParamsStore, globalState => globalState.absolute);
 export const makeSelectFilterParams = () =>
   createSelector([globalSelectors.makeSelectStartDate(), globalSelectors.makeSelectEndDate()], (startDate, endDate) => ({
     fromDate: startDate.unix() * 1000.0,
