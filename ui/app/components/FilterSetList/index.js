c3bdfaa2c17c0467441344c4ac441090f14168be
Stoyan Atanasoff <sa@stamus-networks.com>
2022-08-03 17:58:23 +0300

    ui: redesigned filter set item

--- ui/app/components/FilterSetList/index.js
+++ ui/app/components/FilterSetList/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Menu, Dropdown, Space, Card } from 'antd';
+import { Space, Card, Tooltip, Button } from 'antd';
 import {
   BellOutlined,
   DashboardOutlined,
   IdcardOutlined,
   SafetyOutlined,
   UploadOutlined,
-  MenuOutlined,
+  CaretRightOutlined,
   InfoCircleOutlined,
   DeleteOutlined,
-  ImportOutlined,
 } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -38,8 +37,14 @@ const getIcons = item => {
 const Description = styled.div`
   font-size: 12px;
   color: #7a7a7a;
+  margin-bottom: 10px;
 `;
 
+const FilterSetFooter = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+`;
 const FilterSetList = ({ item, info, loadFilterSets, deleteFilterSet, noRights }) => (
   <Card
     size="small"
@@ -51,34 +56,29 @@ const FilterSetList = ({ item, info, loadFilterSets, deleteFilterSet, noRights }
     }
     headStyle={{ background: '#efefef' }}
     extra={
-      <Dropdown
-        getPopupContainer={() => document.getElementById('container')}
-        overlay={
-          <Menu>
-            <Menu.Item icon={<ImportOutlined />} onClick={() => loadFilterSets(item)}>
-              Load
-            </Menu.Item>
-            {!noRights && deleteFilterSet && (
-              <Menu.Item icon={<DeleteOutlined />} key="delete" onClick={() => deleteFilterSet('global', item)}>
-                Delete
-              </Menu.Item>
-            )}
-          </Menu>
-        }
-        trigger={['hover']}
-      >
-        <div id="container">
-          <MenuOutlined />
-        </div>
-      </Dropdown>
+      <>
+        {!noRights && deleteFilterSet && (
+          <Tooltip title="Delete" getPopupContainer={() => document.getElementById('container')}>
+            <Button size="small" type="danger" icon={<DeleteOutlined />} onClick={() => deleteFilterSet('global', item)} />
+          </Tooltip>
+        )}
+      </>
     }
   >
+    <div id="container" />
     <Description>{item.description}</Description>
-    <Space split="|">
-      <span>{`${item.pageTitle} Page`}</span>
-      <span>Shared</span>
-      {!info && <InfoCircleOutlined />}
-    </Space>
+    <FilterSetFooter>
+      <Space>
+        <Tooltip title="Load" getPopupContainer={() => document.getElementById('container')}>
+          <Button size="small" type="primary" icon={<CaretRightOutlined />} onClick={() => loadFilterSets(item)} />
+        </Tooltip>
+      </Space>
+      <Space split="|">
+        <span>{`${item.pageTitle} Page`}</span>
+        <span>Shared</span>
+        {!info && <InfoCircleOutlined />}
+      </Space>
+    </FilterSetFooter>
   </Card>
 );
 
