aae386ef4fd25f406fe246bd26a20e33b3f19459
Tzvetan Marinov <tmarinov@stamus-networks.com>
2022-08-23 15:46:50 +0300

    ui: fixed missing links in leftnav

--- ui/app/pages/Administration/Administration.js
+++ ui/app/pages/Administration/Administration.js
@@ -6,22 +6,23 @@ export const Other = () => null;
 Appliances.metadata = {
   category: 'ADMINISTRATION',
   url: () => '/suricata',
+  title: 'Suricata',
   // eslint-disable-next-line camelcase
-  access: permissions => !!permissions?.rules?.configuration_view,
+  access: permissions => !!permissions.indexOf('rules.configuration_view') !== -1,
 };
 
 Users.metadata = {
   category: 'ADMINISTRATION',
   url: () => '/accounts',
   // eslint-disable-next-line camelcase
-  access: permissions => !!permissions?.rules?.configuration_auth,
+  access: permissions => !!permissions.indexOf('rules.configuration_auth') !== -1,
 };
 
 Sources.metadata = {
   category: 'ADMINISTRATION',
   url: () => '/rules/source',
   // eslint-disable-next-line camelcase
-  access: permissions => !!permissions?.rules?.source_view,
+  access: permissions => !!permissions.indexOf('rules.source_view') !== -1,
 };
 
 Other.metadata = {
