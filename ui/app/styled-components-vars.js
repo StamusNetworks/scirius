9625a733551e2dcd7f08db1555443259c1a41d95
Tzvetan Marinov <tmarinov@stamus-networks.com>
2023-01-18 19:30:09 +0200

    ui: kill chains colors updates

--- ui/app/styled-components-vars.js
+++ ui/app/styled-components-vars.js
@@ -2,10 +2,10 @@ export const screenXXL = '1600px';
 export const screenXL = '1200px';
 
 // Kill Chain colors
-export const actionsOnObjectivesColor = '#750512';
-export const commandAndControlColor = '#815517';
-export const deliveryColor = '#bba343';
-export const exploitationColor = '#ffc000';
-export const installationColor = '#d88a13';
-export const reconnaissanceColor = '#005792';
-export const weaponizationColor = '#728568';
+export const actionsOnObjectivesColor = { rgb: 'rgb(117, 5, 18)', rgba: 'rgba(117, 5, 18, 0.3)' };
+export const commandAndControlColor = { rgb: 'rgb(129, 85, 23)', rgba: 'rgba(129, 85, 23, 0.3)' };
+export const deliveryColor = { rgb: 'rgb(187, 163, 67)', rgba: 'rgba(187, 163, 67, 0.3)' };
+export const exploitationColor = { rgb: 'rgb(255, 192, 0)', rgba: 'rgba(255, 192, 0, 0.3)' };
+export const installationColor = { rgb: 'rgb(216, 138, 19)', rgba: 'rgba(216, 138, 19, 0.3)' };
+export const reconnaissanceColor = { rgb: 'rgb(0, 87, 146)', rgba: 'rgba(0, 87, 146, 0.3)' };
+export const weaponizationColor = { rgb: 'rgb(114, 133, 104)', rgba: 'rgba(114, 133, 104, 0.3)' };
