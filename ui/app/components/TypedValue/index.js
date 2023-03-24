dd6a38ffbaf365b4fc823fd87359b525c873d6f2
Tzvetan Marinov <tmarinov@stamus-networks.com>
2023-03-24 11:50:38 +0200

    ui: introduced new links for IP

--- ui/app/components/TypedValue/index.js
+++ ui/app/components/TypedValue/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'ui/helpers/Link';
+import { connect } from 'react-redux';
 import { Dropdown } from 'antd';
-import { IdcardOutlined } from '@ant-design/icons';
+import { IdcardOutlined, ZoomInOutlined, ZoomOutOutlined } from '@ant-design/icons';
 import _ from 'lodash';
+import { Link } from 'ui/helpers/Link';
+import history from 'ui/utils/history';
+import { addFilter } from 'ui/containers/HuntApp/stores/global';
+import { sections } from 'ui/constants';
 // import { IP_FIELDS } from 'components/FilterList/FilterList';
 
 const TypedValue = props => {
@@ -12,7 +16,7 @@ const TypedValue = props => {
   if (props.type === 'ip') {
     listOfLinks = [
       {
-        key: '3',
+        key: 'typedValueIP1',
         label: (
           <Link to={`/stamus/hunting/hosts/host-insight/${props.value}`}>
             <div>
@@ -22,6 +26,44 @@ const TypedValue = props => {
         ),
       },
       ...props.additionalLinks,
+      {
+        key: 'typedValueIP2',
+        label: (
+          <div
+            onClick={() => {
+              props.addFilter(sections.GLOBAL, {
+                id: props.value || '',
+                value: props.value || '',
+                label: `IP: ${props.value}`,
+                fullString: true,
+                negated: false,
+              });
+              if (props.redirect) history.push('/stamus/hunting/dashboards');
+            }}
+          >
+            <ZoomInOutlined /> <span>filter on IP: {props.value}</span>
+          </div>
+        ),
+      },
+      {
+        key: 'typedValueIP3',
+        label: (
+          <div
+            onClick={() => {
+              props.addFilter(sections.GLOBAL, {
+                id: props.value,
+                value: props.value,
+                label: `IP: ${props.value}`,
+                fullString: true,
+                negated: true,
+              });
+              if (props.redirect) history.push('/stamus/hunting/dashboards');
+            }}
+          >
+            <ZoomOutOutlined /> <span>negated filter on IP: {props.value}</span>
+          </div>
+        ),
+      },
     ].filter(obj => !_.isEmpty(obj.label)); // removes the ones that dont have data;
   }
 
@@ -32,7 +74,7 @@ const TypedValue = props => {
       }}
       trigger={['click']}
       onClick={e => {
-        // e.stopPropagation();
+        e.stopPropagation();
       }}
     >
       {props.children || <a>{props.value}</a>}
@@ -47,7 +89,10 @@ TypedValue.defaultProps = {
 TypedValue.propTypes = {
   type: PropTypes.string.isRequired, // 'ip|hostname|username|port'
   value: PropTypes.string.isRequired, // '10.136.0.33',
-  additionalLinks: PropTypes.arrayOf(PropTypes.string),
+  redirect: PropTypes.bool,
+  additionalLinks: PropTypes.arrayOf(PropTypes.object),
+  addFilter: PropTypes.func,
+  children: PropTypes.object,
 };
 
-export default TypedValue;
+export default connect(null, { addFilter })(TypedValue);
