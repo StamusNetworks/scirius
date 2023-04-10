d21acdffb1d6c275e2ce6c7faba0162fb8e54373
Rositsa Kyuchukova <rk@stamus-networks.com>
2023-04-10 12:36:49 +0300

    ui: fixed test id on filter sets names

--- ui/app/components/FilterSetItem/index.js
+++ ui/app/components/FilterSetItem/index.js
@@ -67,7 +67,7 @@ const FilterSetItem = ({ item, loadFilterSets, onDelete, hasRights, loading, typ
       )
     }
   >
-    <Container onClick={() => loadFilterSets(item)} data-test={`filter-set-item-${item.id}`}>
+    <Container onClick={() => loadFilterSets(item)} data-test={`filter-set-item-${item.name}`}>
       {item.description && <Description>{item.description}</Description>}
       <FilterSetFooter data-test={`${huntTabs[item.page]} Page`}>{`${huntTabs[item.page]} Page`}</FilterSetFooter>
     </Container>
