f68a32591f5bd504fefe45e2fd9962491f6e5f69
Stoyan Atanasoff <sa@stamus-networks.com>
2023-05-16 01:45:51 +0300

    ui: fixed useAutorun hook dependencies check

--- ui/app/helpers/useAutorun.js
+++ ui/app/helpers/useAutorun.js
@@ -12,32 +12,34 @@ function useAutorun(callback, dependencies = ['ids', 'date', 'tenant']) {
     [callback],
   );
 
-  return useEffect(() => {
-    return autorun(() => {
-      try {
-        let trigger = false;
-        const params = {};
-        if (dependencies.includes('ids')) {
-          params.ids = commonStore.ids;
-          trigger = true;
-        }
-        if (dependencies.includes('date')) {
-          // console.log('GLOBALS', dependencies);
-          params.startDate = commonStore.startDate;
-          params.endDate = commonStore.endDate;
-          trigger = true;
-        }
-        if (dependencies.includes('tenant')) {
-          params.endDate = tenantStore?.tenant;
-          trigger = true;
-        }
-        if (trigger) {
-          cb(params);
-        }
-        // eslint-disable-next-line no-empty
-      } catch (e) {}
-    });
-  }, []);
+  return useEffect(
+    () =>
+      autorun(() => {
+        try {
+          let trigger = false;
+          const params = {};
+          if (dependencies.includes('ids')) {
+            params.ids = commonStore.ids;
+            trigger = true;
+          }
+          if (dependencies.includes('date')) {
+            // console.log('GLOBALS', dependencies);
+            params.startDate = commonStore.startDate;
+            params.endDate = commonStore.endDate;
+            trigger = true;
+          }
+          if (dependencies.includes('tenant')) {
+            params.endDate = tenantStore?.tenant;
+            trigger = true;
+          }
+          if (trigger || dependencies.length === 0) {
+            cb(params);
+          }
+          // eslint-disable-next-line no-empty
+        } catch (e) {}
+      }),
+    [],
+  );
 }
 
 export default useAutorun;
