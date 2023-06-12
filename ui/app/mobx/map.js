b3296e78cdb382d584b35a6bd20205e82104240b
Tzvetan Marinov <tmarinov@stamus-networks.com>
2023-06-12 10:29:08 +0300

    ui: enable event types to be used as query strings

--- ui/app/mobx/map.js
+++ ui/app/mobx/map.js
@@ -6,10 +6,18 @@ const map = type => {
         to_date: localStorage.getItem('startDate'),
       };
     }
+    // should be used for all `/es/*` requests
     case ':datesEs': {
       return {
         from_date: localStorage.getItem('startDate') * 1000,
         to_date: localStorage.getItem('endDate') * 1000,
+      };
+    }
+    case ':eventTypes': {
+      return {
+        alert: JSON.parse(localStorage.getItem('alert_tag'))?.value?.alerts,
+        discovery: !!JSON.parse(localStorage.getItem('alert_tag'))?.value?.sightings,
+        stamus: JSON.parse(localStorage.getItem('alert_tag'))?.value?.stamus,
       };
     }
     default:
