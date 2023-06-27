885e9b77a490d4c97d894e3566d23858da92c8df
Stoyan Atanasoff <sa@stamus-networks.com>
2023-06-27 23:05:13 +0300

    ui: useFilterParams fixed dates in milliseconds

--- ui/app/hooks/useFilterParams.js
+++ ui/app/hooks/useFilterParams.js
@@ -2,7 +2,7 @@ import { useStore } from 'ui/mobx/RootStoreProvider';
 
 const useFilterParams = () => {
   const { commonStore } = useStore();
-  return `from_date=${commonStore.startDate}&to_date=${commonStore.endDate}`;
+  return `from_date=${commonStore.startDate * 1000}&to_date=${commonStore.endDate * 1000}`;
 };
 
 export default useFilterParams;
