a2d79fa81257dc3d05ed743e3040c78f8cae7d05
Stoyan Atanasoff <sa@stamus-networks.com>
2023-06-28 11:19:58 +0300

    ui: Refresh component connected to mobx

--- ui/app/components/Refresh/index.js
+++ ui/app/components/Refresh/index.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import { Button, Space, Select, Tooltip } from 'antd';
-import PropTypes from 'prop-types';
 import { ReloadPeriodEnum } from 'ui/maps/ReloadPeriodEnum';
 import { ReloadOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
+import { observer } from 'mobx-react-lite';
+import { useStore } from 'ui/mobx/RootStoreProvider';
 
 const SpaceStyled = styled(Space)`
   margin-left: 10px;
   padding-top: 5px;
 `;
 
-const Refresh = ({ onChange, onRefresh }) => (
-  <SpaceStyled>
-    <Select onChange={value => onChange(ReloadPeriodEnum[value])} defaultValue="NONE" style={{ width: 160 }}>
-      {Object.keys(ReloadPeriodEnum).map(p => (
-        <Select.Option key={p} value={p}>
-          {ReloadPeriodEnum[p].title}
-        </Select.Option>
-      ))}
-    </Select>
-    <Tooltip title="Reload now">
-      <Button onClick={() => onRefresh()} icon={<ReloadOutlined />} />
-    </Tooltip>
-  </SpaceStyled>
-);
-
-export default Refresh;
-
-Refresh.propTypes = {
-  onChange: PropTypes.func,
-  onRefresh: PropTypes.func,
+const Refresh = () => {
+  const { commonStore } = useStore();
+  return (
+    <SpaceStyled>
+      <Select onChange={value => commonStore.setRefreshTime(ReloadPeriodEnum[value])} defaultValue="NONE" style={{ width: 160 }}>
+        {Object.keys(ReloadPeriodEnum).map(p => (
+          <Select.Option key={p} value={p}>
+            {ReloadPeriodEnum[p].title}
+          </Select.Option>
+        ))}
+      </Select>
+      <Tooltip title="Reload now">
+        <Button onClick={() => commonStore.reload()} icon={<ReloadOutlined />} />
+      </Tooltip>
+    </SpaceStyled>
+  );
 };
+
+export default observer(Refresh);
