4133c2dc4c6c37b63dd33c5c1f5761140ea99409
Stoyan Atanasoff <sa@stamus-networks.com>
2023-07-22 17:14:40 +0300

    ui: fixed url user path

--- ui/app/config.js
+++ ui/app/config.js
@@ -2,5 +2,5 @@ export const API_URL = '/rest/appliances';
 export const RULES_URL = '/rest/rules';
 export const APP_URL = '/stamus';
 export const HUNT_URL = '/rules/hunt';
-export const CURRENT_USER_PATH = '/rest/accounts/sciriususer/current_user';
+export const CURRENT_USER_PATH = '/rest/accounts/sciriususer/current_user/';
 export const STAMUS = 'Stamus';
