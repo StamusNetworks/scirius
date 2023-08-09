53f9fc856f3e709160447df5d6a78c113af48d59
Stoyan Atanasoff <sa@stamus-networks.com>
2023-08-09 12:42:57 +0300

    ui: create a filter function for invisible / visible dashboard panel

--- ui/app/components/DashboardMosaic/index.js
+++ ui/app/components/DashboardMosaic/index.js
@@ -4,6 +4,8 @@ import DashboardPanel from 'ui/components/DashboardPanel';
 import useKeyPress from 'ui/hooks/useKeyPress';
 import { createGlobalStyle } from 'styled-components';
 import DashboardContext from 'ui/context/DashboardContext';
+import { observer } from 'mobx-react-lite';
+import { useStore } from 'ui/mobx/RootStoreProvider';
 
 const GlobalStyle = createGlobalStyle`
   .item-enter {
@@ -27,17 +29,28 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const DashboardMosaic = () => {
+  const { commonStore } = useStore();
   const ctrl = useKeyPress('Control');
+
+  const isVisible = panelId => {
+    const callbacks = {
+      discovery: () => commonStore.eventTypes.discovery,
+    };
+    return callbacks[panelId] ? callbacks[panelId]() : true;
+  };
+
   return (
     <div data-test="dashboard-mosaic">
       <GlobalStyle />
       <DashboardContext.Provider value={ctrl}>
-        {Object.keys(dashboard).map(panelId => (
-          <DashboardPanel panelId={panelId} />
-        ))}
+        {Object.keys(dashboard)
+          .filter(isVisible)
+          .map(panelId => (
+            <DashboardPanel panelId={panelId} />
+          ))}
       </DashboardContext.Provider>
     </div>
   );
 };
 
-export default DashboardMosaic;
+export default observer(DashboardMosaic);
