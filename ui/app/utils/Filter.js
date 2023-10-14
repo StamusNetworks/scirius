a0146a972399850a5c03f1633f7b2a968d1a6071
Stoyan Atanasoff <sa@stamus-networks.com>
2023-10-14 03:18:33 +0300

    ui: Created Filter class

---
+++ ui/app/utils/Filter.js
@@ -0,0 +1,115 @@
+import { FilterCategory, FiltersList, FilterType } from 'ui/maps/Filters';
+
+export default class Filter {
+  #_filter = null;
+
+  /**
+   * Filter default properties
+   * @type {{negated: boolean, fullString: boolean, label: null, title: null, category: string, type: string, value: null, id: null}}
+   */
+  #_defaults = {
+    /* Filter id. Example: alert.severity */
+    id: null,
+    /* Filter raw value. Example: 1 */
+    value: null,
+    /* Filter title. Example: Severity */
+    title: null,
+    /* Filter label in filter bar. Example: Severity: Critical */
+    label: null,
+    /* Filter category. Example: event | host */
+    category: FilterCategory.event,
+    /* Filter type: IP | PORT | MITRE | USERNAME | HOSTNAME | ROLE | GENERIC */
+    type: FilterType.GENERIC,
+    /* Filter mode: including or excluding. Example: true | false */
+    negated: false,
+    /* Filter mode: exact match. Example: true | false */
+    fullString: false,
+  };
+
+  constructor(filter, value, props) {
+    this.#_defaults = Object.assign(this.#_defaults, props);
+    this.#_filter = this.#makeFilter(filter, value, props);
+    return this;
+  }
+
+  valueOf() {
+    return this.#_filter;
+  }
+
+  toString() {
+    return JSON.stringify(this.#_filter);
+  }
+
+  #makeFilter(filterKey, value, props) {
+    const filterSchema = FiltersList.find(f => f.id === filterKey);
+    if (filterSchema) {
+      return {
+        ...filterSchema,
+        label: `${props?.title || filterSchema.title || filterKey}: ${filterSchema.format?.(value) || value}`,
+        value,
+        negated: props?.negated || false,
+        fullString: props?.fullString !== undefined ? props.fullString : true,
+        convertible: filterSchema.convertible || false,
+      };
+    }
+    return {
+      /* default schema */
+      title: props?.title || filterKey,
+      id: filterKey,
+      category: FilterCategory.event,
+      type: FilterType.GENERIC,
+      /* default filter props */
+      value,
+      label: `${filterKey}: ${value}`,
+      negated: props?.negated || false,
+      fullString: props?.fullString || true,
+    };
+  }
+
+  get instance() {
+    return this.#_filter;
+  }
+
+  get value() {
+    return this.#_filter.value;
+  }
+
+  get displayValue() {
+    return this.#_filter.format ? this.#_filter.format(this.#_filter.value) : this.#_filter.value;
+  }
+
+  get id() {
+    return this.#_filter.id;
+  }
+
+  get label() {
+    return this.#_filter.label;
+  }
+
+  get convertible() {
+    return !!this.#_filter.convertible;
+  }
+
+  get negated() {
+    return !!this.#_filter.negated;
+  }
+
+  get fullString() {
+    return !!this.#_filter.fullString;
+  }
+
+  negate(value) {
+    this.#_filter.negated = value || true;
+    return this;
+  }
+
+  setValue(v) {
+    this.value = v;
+    return this;
+  }
+
+  convert() {
+    this.#_filter = this.#makeFilter(this.#_filter.convertible, this.value, { negated: this.#_filter.negated, fullString: this.#_filter.fullString });
+    return this;
+  }
+}
