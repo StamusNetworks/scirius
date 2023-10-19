158c3b231c9687d671850e2d27f04f2e892fd76b
Stoyan Atanasoff <sa@stamus-networks.com>
2023-10-19 12:33:00 +0300

    ui: exposed filter types to avoid circular dependency injection

---
+++ ui/app/maps/FiltersFieldTypes.js
@@ -0,0 +1,16 @@
+export const INTERGER_FIELDS_ENDS_WITH = ['.min', '.max', '_min', '_max', '.port', '_port', '.length'];
+
+export const INTERGER_FIELDS_EXACT = [
+  'alert.signature_id',
+  'alert.rev',
+  'alert.severity',
+  'http.status',
+  'vlan',
+  'flow_id',
+  'flow.bytes_toclient',
+  'flow.bytes_toserver',
+  'flow.pkts_toclient',
+  'flow.pkts_toserver',
+  'geoip.provider.autonomous_system_number',
+  'port',
+];
