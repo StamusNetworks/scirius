2a36fdd0b646709040518f98ebcdc3775f213afa
Stoyan Atanasoff <sa@stamus-networks.com>
2023-12-14 21:56:17 +0200

    ui: cleared unused code from internals/templates

--- ui/internals/templates/app.js
+++ ui/internals/templates/app.js
@@ -9,7 +9,6 @@ import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
 import history from 'utils/history';
 
 // Import root app
@@ -38,9 +37,7 @@ const render = messages => {
   ReactDOM.render(
     <Provider store={store}>
       <LanguageProvider messages={messages}>
-        <ConnectedRouter history={history}>
-          <App />
-        </ConnectedRouter>
+        <App />
       </LanguageProvider>
     </Provider>,
     MOUNT_NODE,
