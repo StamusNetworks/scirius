8c884d22793eb6efba38e4cdf354ca15d034ca4c
mkurz <mkurz@stamus-networks.com>
2024-01-17 14:28:22 +0100

    ui: add feature to hide empty tiles on Hunting Dashboard

--- ui/app/components/DashboardMosaic/index.js
+++ ui/app/components/DashboardMosaic/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Switch, Row, Empty } from 'antd';
+import styled, { createGlobalStyle } from 'styled-components';
+
 import { dashboard } from 'ui/config/Dashboard';
 import DashboardPanel from 'ui/components/DashboardPanel';
 import useKeyPress from 'ui/hooks/useKeyPress';
-import { createGlobalStyle } from 'styled-components';
 import DashboardContext from 'ui/context/DashboardContext';
-import { observer } from 'mobx-react-lite';
 import { useStore } from 'ui/mobx/RootStoreProvider';
+import UISwitchLabel from 'ui/components/UIElements/UISwitchLabel';
+import UICard from 'ui/components/UIElements/UICard';
 
 const GlobalStyle = createGlobalStyle`
   .item-enter {
@@ -28,6 +32,10 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const Card = styled(UICard)`
+  margin-top: 0.5rem;
+`;
+
 const DashboardMosaic = () => {
   const { commonStore } = useStore();
   const ctrl = useKeyPress('Control');
@@ -40,13 +48,55 @@ const DashboardMosaic = () => {
     return callbacks[panelId] ? callbacks[panelId]() : true;
   };
 
+  const localHideEmptyTiles = JSON.parse(localStorage.getItem('hide-empty-tiles'));
+
+  const [hideEmptyTiles, setHideEmptyTiles] = useState(localHideEmptyTiles ?? true);
+
+  const updateHideEmptyTiles = useCallback(hideEmptyTilesState => {
+    setHideEmptyTiles(hideEmptyTilesState);
+    localStorage.setItem('hide-empty-tiles', hideEmptyTilesState);
+  }, []);
+
+  const [emptyPanelsList, setEmptyPanelsList] = useState([]);
+  const setPanelIsEmpty = useCallback(
+    (panelId, isEmpty) => setEmptyPanelsList(prev => (isEmpty ? [...prev, panelId] : prev.filter(id => id !== panelId))),
+    [setEmptyPanelsList],
+  );
+  const dashboardIsEmpty = useMemo(
+    () => dashboard.filter(isVisible).every(panel => emptyPanelsList.includes(panel.panelId)),
+    [
+      emptyPanelsList,
+      dashboard
+        .filter(isVisible)
+        .map(panel => panel.panelId)
+        .join(' '),
+    ],
+  );
+
   return (
     <div data-test="dashboard-mosaic">
       <GlobalStyle />
+      <Row justify="end" align="middle">
+        <Switch
+          data-test="hide-empty-tiles-switch"
+          size="small"
+          checkedChildren="ON"
+          unCheckedChildren="OFF"
+          checked={hideEmptyTiles}
+          onChange={() => updateHideEmptyTiles(!hideEmptyTiles)}
+        />{' '}
+        <UISwitchLabel style={{ marginLeft: '4px' }}>Hide empty tiles</UISwitchLabel>
+      </Row>
+
       <DashboardContext.Provider value={ctrl}>
         {dashboard.filter(isVisible).map(panel => (
-          <DashboardPanel panel={panel} />
+          <DashboardPanel panel={panel} hideEmptyTiles={hideEmptyTiles} setPanelIsEmpty={setPanelIsEmpty} />
         ))}
+        {hideEmptyTiles && dashboardIsEmpty && (
+          <Card>
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No matching data found for the applied filters." />
+          </Card>
+        )}
       </DashboardContext.Provider>
     </div>
   );
