9dc82c58bf328fdbe8186cd2428b47cf78bbbeb4
Mathias Kurz <mkurz@stamus-networks.com>
2024-02-09 10:56:36 +0100

    ui: add useWindowWidth hook

---
+++ ui/app/hooks/useWindowWidth.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+export default function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowWidth;
+}
