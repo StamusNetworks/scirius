2b5453dcc3a79c2e0467c2692615964754696d26
Mathias Kurz <mkurz@stamus-networks.com>
2024-04-03 17:06:18 +0200

    ui: add new Timeline to Dashboard

---
+++ ui/app/pages/Dashboards/components/Timeline/Timeline.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+import PropTypes from 'prop-types';
+
+import { buildQFilter } from 'ui/buildQFilter';
+import { BarChart, XAxisLabelDateTime } from 'ui/components/Charts';
+import { getTimelineData } from 'ui/components/Charts/helpers';
+import notify from 'ui/helpers/notify';
+import useAutorun from 'ui/helpers/useAutorun';
+import { useStore } from 'ui/mobx/RootStoreProvider';
+
+import * as Style from './style';
+
+export const Timeline = ({ filterParams, chartTarget, filters, systemSettings, eventTypes }) => {
+  const { esStore } = useStore();
+  const [loading, setLoading] = useState(true);
+  const [chart, setChart] = useState({});
+
+  const fetchData = async () => {
+    try {
+      const qfilter = buildQFilter(filters, systemSettings);
+      const res = await esStore.fetchTimeline(chartTarget, qfilter);
+      setChart(getTimelineData(res, chartTarget));
+      setLoading(false);
+    } catch (e) {
+      notify('Error fetching data', e);
+      setLoading(false);
+    }
+  };
+
+  useAutorun(fetchData, [filterParams, chartTarget, filters, systemSettings, eventTypes]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Style.Card flex>
+      <BarChart chart={chart} stacked XAxisLabel={XAxisLabelDateTime} />
+    </Style.Card>
+  );
+};
+
+Timeline.propTypes = {
+  filterParams: PropTypes.object.isRequired,
+  chartTarget: PropTypes.string.isRequired,
+  filters: PropTypes.array.isRequired,
+  systemSettings: PropTypes.object.isRequired,
+  eventTypes: PropTypes.array.isRequired,
+};
