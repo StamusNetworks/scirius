dd8a8c3ff861b31797270c6f8d8b33f47f00e77e
Mathias Kurz <mkurz@stamus-networks.com>
2024-04-03 15:22:23 +0200

    ui: add new signature display

--- ui/app/assets/icons/index.js
+++ ui/app/assets/icons/index.js
@@ -1,1 +1,5 @@
+export { ArrowIcon } from './arrow';
+export { ClientIcon } from './client';
 export { ReadMoreIcon } from './read_more';
+export { ServerIcon } from './server';
+export { TargetIcon } from './target';
