4a000f175ab83b29f80d29b162aec53147109eda
Mathias Kurz <mkurz@stamus-networks.com>
2024-04-08 15:43:03 +0200

    ui: rework assets folder structure

--- ui/app/components/index.js
+++ ui/app/components/index.js
@@ -3,5 +3,4 @@ export { default as Sider } from './Sider/Sider';
 export { default as Content } from './Content/Content';
 export { default as Link } from './Link/Link';
 export { default as LinkGroup } from './LinkGroup/LinkGroup';
-export { default as LinkGroupTitle } from './LinkGroupTitle/LinkGroupTitle';
 export { default as SideLink } from './SideLink/SideLink';
