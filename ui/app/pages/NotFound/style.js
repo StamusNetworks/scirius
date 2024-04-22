83b28bea51ae864e61e47c8ab2eb3c1b32e5c270
Mathias Kurz <mkurz@stamus-networks.com>
2024-04-22 10:42:40 +0200

    ui: improve 404 page

---
+++ ui/app/pages/NotFound/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  height: 100%;
+`;
