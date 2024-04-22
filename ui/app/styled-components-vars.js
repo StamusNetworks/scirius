6d158925916e9a2f7e099ba8bd43f4e93401b988
Mathias Kurz <mkurz@stamus-networks.com>
2024-04-22 11:44:57 +0200

    ui: add Policy Violation to kill chain map

--- ui/app/styled-components-vars.js
+++ ui/app/styled-components-vars.js
@@ -9,3 +9,4 @@ export const exploitationColor = { rgb: 'rgb(255, 192, 0)', rgba: 'rgba(255, 192
 export const installationColor = { rgb: 'rgb(216, 138, 19)', rgba: 'rgba(216, 138, 19, 0.3)' };
 export const reconnaissanceColor = { rgb: 'rgb(0, 87, 146)', rgba: 'rgba(0, 87, 146, 0.3)' };
 export const weaponizationColor = { rgb: 'rgb(114, 133, 104)', rgba: 'rgba(114, 133, 104, 0.3)' };
+export const policyViolationColor = { rgb: 'rgb(60, 60, 60)', rgba: 'rgba(0, 0, 0, 0.3)' };
