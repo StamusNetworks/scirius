71a65d5f56ab9c132893821faa0845ac1ac5c867
Mathias Kurz <mkurz@stamus-networks.com>
2024-04-22 12:54:22 +0200

    ui: add sword icon

--- ui/app/assets/icons/index.js
+++ ui/app/assets/icons/index.js
@@ -2,4 +2,5 @@ export { ArrowIcon } from './arrow';
 export { ClientIcon } from './client';
 export { ReadMoreIcon } from './read_more';
 export { ServerIcon } from './server';
+export { SwordIcon } from './sword';
 export { TargetIcon } from './target';
