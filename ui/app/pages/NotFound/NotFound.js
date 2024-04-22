3720dff8f4c7868c62877aa25dcb7bd30d09c859
Mathias Kurz <mkurz@stamus-networks.com>
2024-04-22 10:42:40 +0200

    ui: improve 404 page

--- ui/app/pages/NotFound/NotFound.js
+++ ui/app/pages/NotFound/NotFound.js
@@ -1,6 +1,34 @@
 import React from 'react';
 
-const NotFound = () => <div>Not found</div>;
+import { Button, Empty } from 'antd';
+import { useHistory } from 'react-router-dom';
+
+import { APP_URL } from 'ui/config';
+import OperationalCenter from 'ui/pages/OperationalCenter/OperationalCenter';
+
+import * as Style from './style';
+
+const NotFound = () => (
+  <Style.PageWrapper>
+    <Empty description={<Description />} />
+  </Style.PageWrapper>
+);
+
+const Description = () => {
+  const history = useHistory();
+  return (
+    <div>
+      <h2>Not found</h2>
+      <p>The page you are trying to access is either missing or contains no data.</p>
+      <Button
+        type="primary"
+        onClick={() => history.push({ pathname: `${APP_URL}/${OperationalCenter.metadata.url}`, search: history.location.search })}
+      >
+        Take me home
+      </Button>
+    </div>
+  );
+};
 
 NotFound.metadata = {
   url: '',
