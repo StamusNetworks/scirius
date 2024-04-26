16f8c5fd1462832684ce95b4d4a1e35786778e03
Mathias Kurz <mkurz@stamus-networks.com>
2024-04-26 10:33:16 +0200

    ui: create API service using apisauce based util

---
+++ ui/app/services/API.js
@@ -0,0 +1,29 @@
+import ENDPOINT from 'ui/config/endpoints';
+import { api } from 'ui/mobx/api';
+
+const API = {
+  // POST
+  setSessionActivity: async (params, options) => api.post(ENDPOINT.SESSION_ACTIVITY.url, params, options),
+  saveFilterSet: async (params, options) => api.post(ENDPOINT.FILTER_SET_SAVE.url, params, options),
+  updatePushRuleset: async () => api.post(ENDPOINT.UPDATE_PUSH_RULESET.url),
+  // GET
+  fetchUser: async () => api.get(ENDPOINT.CURRENT_USER.url),
+  fetchContext: async () => api.get(ENDPOINT.SCIRIUS_CONTEXT.url),
+  fetchSystemSettings: async () => api.get(ENDPOINT.SYSTEM_SETTINGS.url),
+  fetchSources: async params => api.get(ENDPOINT.SOURCES.url, params),
+  fetchAllPeriod: async params => api.get(ENDPOINT.ALL_PERIOD.url, params),
+  fetchRuleSets: async () => api.get(ENDPOINT.RULE_SETS.url),
+  fetchHuntFilter: async () => api.get(ENDPOINT.HUNT_FILTER.url),
+  fetchSupportedActions: async (params, options) => api.post(ENDPOINT.SUPPORTED_ACTIONS.url, params, options),
+  fetchHistoryFilters: async () => api.get(ENDPOINT.HISTORY_FILTERS.url),
+  fetchFilterSets: async () => api.get(ENDPOINT.FILTER_SETS.url),
+  fetchDashboardPanel: async (params, options) => api.get(ENDPOINT.DASHBOARD_PANEL.url, params, options),
+  fetchFieldStats: async (params, options) => api.get(ENDPOINT.FIELD_STATS.url, params, options),
+  fetchTimeline: async (params, options) => api.get(ENDPOINT.TIMELINE.url, params, options),
+  fetchAlertsCount: async (params, options) => api.get(ENDPOINT.ALERTS_COUNT.url, params, options),
+  fetchAlertsTail: async (params, options) => api.get(ENDPOINT.ALERTS_TAIL.url, params, options),
+  // DELETE
+  deleteFilterSet: async (params, options) => api.delete(ENDPOINT.FILTER_SET_DELETE.url, params, options),
+};
+
+export default API;
