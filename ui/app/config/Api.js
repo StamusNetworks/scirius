f3436b3e8182d99915cffcf40af3d018d4a4476e
Aurélie Ambal <aurelie@stamus-networks.com>
2024-05-14 18:12:08 +0200

    ui: Refactor Policies page to improve display of parameters

--- ui/app/config/Api.js
+++ ui/app/config/Api.js
@@ -1,6 +1,5 @@
 export const API_URL = '/';
 export const RULE_PATH = 'rest/rules/rule/';
-export const RULESET_PATH = 'rest/rules/ruleset/';
 export const SOURCE_PATH = 'rest/rules/source/';
 export const CATEGORY_PATH = 'rest/rules/category/';
 export const HISTORY_PATH = 'rules/history/';
