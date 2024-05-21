18180f2585926681cd68f70d4d6d3b052fef6950
Aurélie Ambal <aurelie@stamus-networks.com>
2024-05-21 17:25:34 +0200

    ui: Remove unused files

--- ui/app/helpers/isBooted.js
+++
@@ -1,4 +0,0 @@
-import { StorageEnum } from 'ui/maps/StorageEnum';
-
-export const isBooted = () => sessionStorage.getItem(StorageEnum.BOOT) || false;
-export const setBooted = value => sessionStorage.setItem(StorageEnum.BOOT, value);
