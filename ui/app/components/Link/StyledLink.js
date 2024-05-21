6a9676f544ef162ec09ea79e052364385c2f46de
Aurélie Ambal <aurelie@stamus-networks.com>
2024-05-21 17:25:34 +0200

    ui: Remove unused files

--- ui/app/components/Link/StyledLink.js
+++
@@ -1,6 +0,0 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-export default styled(Link)`
-  color: #222;
-`;
--- ui/app/helpers/NormalToDashCase.js
+++
@@ -1,9 +0,0 @@
-/**
- * Converts normal string with spaces to dash-case
- *
- * @example "Normal String Here" to "normal-string-here"
- */
-
-export default function (value) {
-  return value.replace(/\s+/g, '-').toLowerCase();
-}
