b65915c0f3c16bc72aedc3f6ca5dd71a82ba0d02
Aurélie Ambal <aurelie@stamus-networks.com>
2024-05-21 17:25:34 +0200

    ui: Remove unused files

--- ui/app/helpers/index.js
+++ ui/app/helpers/index.js
@@ -1,4 +1,2 @@
 export { default as CamelCaseToDashCase } from './CamelCaseToDashCase';
-export { default as NormalToCamelCase } from './NormalToCamelCase';
 export { default as CamelCaseToNormal } from './CamelCaseToNormal';
-export { default as NormalToDashCase } from './NormalToDashCase';
