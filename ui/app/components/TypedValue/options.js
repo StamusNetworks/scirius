f88d80ea93e3e69269b458cf167760a6110697a6
Stoyan Atanasoff <sa@stamus-networks.com>
2024-05-28 12:24:36 +0300

    ui: exposed reusable typed value options

---
+++ ui/app/components/TypedValue/options.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { CopyOutlined, InfoCircleFilled, ZoomInOutlined, ZoomOutOutlined } from '@ant-design/icons';
+import { message } from 'antd';
+
+import copyTextToClipboard from 'ui/helpers/copyTextToClipboard';
+import isIP from 'ui/helpers/isIP';
+
+const CETypedOptions = {
+  COPY_TO_CLIPBOARD: value => ({
+    key: 'copyTextToClipboard',
+    label: (
+      <div
+        onClick={() => {
+          copyTextToClipboard(value);
+          message.success({
+            duration: 1,
+            content: 'Copied!',
+          });
+        }}
+      >
+        <CopyOutlined /> <span>Copy text to clipboard</span>
+      </div>
+    ),
+  }), // Copy text to clipboard
+  FILTER_ON_IP: (displayValue, onClick) => ({
+    key: 'typedValueIP2',
+    label: (
+      <div onClick={onClick}>
+        <ZoomInOutlined /> <span>Filter on IP: {displayValue}</span>
+      </div>
+    ),
+  }), // Filter on IP: 0.0.0.0
+  NEGATED_FILTER_ON_IP: (displayValue, onClick) => ({
+    key: 'typedValueIP3',
+    label: (
+      <div onClick={onClick}>
+        <ZoomOutOutlined /> <span>Negated filter on IP: {displayValue}</span>
+      </div>
+    ),
+  }), // Negated filter on IP: 0.0.0.0
+  EXTERNAL_INFO: value => ({
+    key: 'typedValueIP4',
+    label: (
+      <a href={`https://www.virustotal.com/gui/${isIP(encodeURIComponent(value)) ? 'ip-address' : 'domain'}/${value}`} target="_blank">
+        <InfoCircleFilled /> <span>External info</span>
+      </a>
+    ),
+  }),
+  EXTERNAL_INFO_PORT: value => ({
+    key: 'typedValuePort',
+    label: (
+      <a href={`https://www.dshield.org/port.html?port=${value}`} target="_blank">
+        <div>
+          <InfoCircleFilled /> <span>External info</span>
+        </div>
+      </a>
+    ),
+  }), // External port info
+};
+
+export default CETypedOptions;
