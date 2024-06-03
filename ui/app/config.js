7cdb7e81c40df0a6629a44b0a9108175d9708640
Stoyan Atanasoff <sa@stamus-networks.com>
2024-06-03 10:41:01 +0300

    ui: added HOME_PATH to config.js

--- ui/app/config.js
+++ ui/app/config.js
@@ -4,3 +4,4 @@ export const APP_URL = '/stamus';
 export const HUNT_URL = '/rules/hunt';
 export const CURRENT_USER_PATH = '/rest/accounts/sciriususer/current_user/';
 export const STAMUS = 'Stamus';
+export const HOME_PATH = '/stamus';
