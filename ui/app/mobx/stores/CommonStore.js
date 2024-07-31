e7d79bc149d6336c1eb62d3ffa0095a103e82f00
Mathias Kurz <mkurz@stamus-networks.com>
2024-07-31 12:17:23 +0200

    ui: add optional time filter for ES search query

--- ui/app/mobx/stores/CommonStore.js
+++ ui/app/mobx/stores/CommonStore.js
@@ -6,9 +6,9 @@ import moment from 'moment';
 import endpoints from 'ui/config/endpoints';
 import Filter from 'ui/utils/Filter';
 
+import { createFilterInstanceFromStorage, getEventTypesToTurnOn, getFilters } from './CommonStore.util';
 import { PeriodEnum } from '../../maps/PeriodEnum';
 import { api } from '../api';
-import { createFilterInstanceFromStorage, getEventTypesToTurnOn, getFilters } from './CommonStore.util';
 
 class CommonStore {
   root = null;
@@ -306,8 +306,8 @@ class CommonStore {
     }
   }
 
-  async fetchElasticSearch(params) {
-    const response = await api.post(endpoints.ELASTIC_SEARCH.url, params);
+  async fetchElasticSearch(params, timeFilter) {
+    const response = await api.post(endpoints.ELASTIC_SEARCH.url, { ...params, time_filter: timeFilter });
     return response;
   }
 
