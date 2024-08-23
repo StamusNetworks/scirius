0cea1cf74c1531b59622b710e03f053c8ab0b738
Mathias Kurz <mkurz@stamus-networks.com>
2024-08-23 14:49:49 +0200

    ui: add link to contextual menu

--- ui/app/pages/Linking/CreateModal/CreateModal.js
+++ ui/app/pages/Linking/CreateModal/CreateModal.js
@@ -24,16 +24,19 @@ export const CreateModal = () => {
       </Button>
       <Modal
         open={open}
-        title="Edit this template"
+        title="Create new template"
         okButtonProps={{ loading: deleteLoading }}
-        okText={deleteLoading ? 'Deleting...' : 'Confirm'}
+        okText={deleteLoading ? 'Saving...' : 'Confirm'}
         cancelText="Cancel"
         onOk={handleDeleteThreat}
         onCancel={handleClose}
       >
         <Form layout="vertical" autoComplete="off">
+          <Form.Item label="Label" name="label">
+            <Input type="text" placeholder="Google" />
+          </Form.Item>
           <Form.Item label="Entity" name="entity">
-            <Select options={[{ value: 'IP' }, { value: 'Threat' }, { value: 'Signature ID' }]} />
+            <Select options={[{ value: 'IP' }, { value: 'Threat' }, { value: 'Signature ID' }]} mode="multiple" />
           </Form.Item>
           <Form.Item label="Template" name="template">
             <Input type="text" placeholder="https://www.google.com/search?q={{ value }}" />
