4ffb6272747ddf56a9f351f8d8ecbc9b0597a9c4
Mathias Kurz <mkurz@stamus-networks.com>
2024-08-29 18:20:21 +0200

    ui: implement backend endpoint for creating and listing templates

--- ui/app/pages/Linking/Linking.js
+++ ui/app/pages/Linking/Linking.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Button, Col, Row, Table } from 'antd';
+import { Col, Row, Table } from 'antd';
 
 import UIBreadcrumb from 'ui/components/UIElements/UIBreadcrumb';
 import { Link } from 'ui/helpers/Link';
+import useAutorun from 'ui/helpers/useAutorun';
+import { useStore } from 'ui/mobx/RootStoreProvider';
+import API from 'ui/services/API';
 
-import { columns } from './Linking.columns';
 import { CreateModal } from './CreateModal/CreateModal';
+import { columns } from './Linking.columns';
 
 const dummyData = [
   {
@@ -17,26 +20,45 @@ const dummyData = [
   },
 ];
 
-const Linking = () => (
-  <div>
-    <UIBreadcrumb
-      items={[
-        'Administration',
-        <Link app to="administration/linking">
-          Linking
-        </Link>,
-      ]}
-    />
-    <Col span={24}>
-      <h1>Contextual DeepLinking</h1>
-      <p>Create templates in order to add links in the contextual menu when right clicking on different values</p>
-    </Col>
-    <Row style={{ marginTop: '1.5rem', marginBottom: '1rem' }}>
-      <CreateModal />
-    </Row>
-    <Table dataSource={dummyData} columns={columns} />
-  </div>
-);
+const Linking = () => {
+  const { commonStore } = useStore();
+  const [deeplinks, setDeeplinks] = useState(null);
+
+  const fetchDeeplinks = async () => {
+    const response = await API.fetchDeeplinks();
+    if (response.ok) {
+      setDeeplinks(response.data?.results);
+    }
+  };
+
+  useAutorun(fetchDeeplinks, ['tenant']);
+
+  const handleSuccess = async () => {
+    await fetchDeeplinks();
+    commonStore.fetchLinkTemplates();
+  };
+
+  return (
+    <div>
+      <UIBreadcrumb
+        items={[
+          'Administration',
+          <Link app to="administration/linking">
+            Linking
+          </Link>,
+        ]}
+      />
+      <Col span={24}>
+        <h1>Contextual DeepLinking</h1>
+        <p>Create templates in order to add links in the contextual menu when right clicking on different values</p>
+      </Col>
+      <Row style={{ marginTop: '1.5rem', marginBottom: '1rem' }}>
+        <CreateModal onSuccess={handleSuccess} />
+      </Row>
+      <Table dataSource={deeplinks} columns={columns} />
+    </div>
+  );
+};
 Linking.metadata = {
   category: 'ADMINISTRATION',
   url: 'administration/linking',
