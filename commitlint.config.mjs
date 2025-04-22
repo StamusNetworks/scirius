0527b61dda355334fa8fbed8434f0fa1dabbaf34
lanath <valentin@viviersoft.com>
2025-04-22 09:58:13 +0200

    chore: adding setup for commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,22 @@
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test',
+			],
+		],
+	},
+}
--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; with python311Packages; [ python3 pip wheel python-ldap mypy ruff ];
+  nativeBuildInputs = with pkgs.buildPackages; with python311Packages; [ python3 pip wheel python-ldap mypy ruff nodejs_18 commitlint ];
 }
