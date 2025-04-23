ff22590a1715dc4b7957e6d3baf7b4af10cbcd28
Eric Leblond <el@stamus-networks.com>
2025-04-23 18:45:00 +0200

    feat(rules): add more types to IOCs
    These types are available in MISP.

--- rules/ioc_mapping.py
+++ rules/ioc_mapping.py
@@ -27,5 +27,37 @@ IOC_MAPPING = {
         'signatures': [
             'alert ip $HOME_NET any -> $EXTERNAL_NET any (msg:"IP IOC {name}"; ip.dst; dataset:isset, {name}, type ip, load {name}; metadata:created_at {created_at}, updated_at {updated_at}{metadata}; sid:{sid}; rev:{rev};)',
         ]
+    },
+    'filename': {
+        'type': 'string',
+        'encoding': 'b64',
+        'validator': None,
+        'signatures': [
+            'alert tcp $HOME_NET any -> $EXTERNAL_NET any (msg:"File IOC {name}"; file.name; dataset:isset, {name}, type string, load {name}; metadata:created_at {created_at}, updated_at {updated_at}{metadata}; sid:{sid}; rev:{rev};)',
+        ]
+    },
+    'url': {
+        'type': 'string',
+        'encoding': 'b64',
+        'validator': None,
+        'signatures': [
+            'alert http $HOME_NET any -> $EXTERNAL_NET any (msg:"URL IOC {name}"; http.uri; dataset:isset, {name}, type string, load {name}; metadata:created_at {created_at}, updated_at {updated_at}{metadata}; sid:{sid}; rev:{rev};)',
+        ]
+    },
+    'http-user-agent': {
+        'type': 'string',
+        'encoding': 'b64',
+        'validator': None,
+        'signatures': [
+            'alert http $HOME_NET any -> $EXTERNAL_NET any (msg:"HTTP User Agent IOC {name}"; http.user_agent; dataset:isset, {name}, type string, load {name}; metadata:created_at {created_at}, updated_at {updated_at}{metadata}; sid:{sid}; rev:{rev};)',
+        ]
+    },
+    'http-cookie': {
+        'type': 'string',
+        'encoding': 'b64',
+        'validator': None,
+        'signatures': [
+            'alert http $HOME_NET any -> $EXTERNAL_NET any (msg:"HTTP Cookie IOC {name}"; http.cookie; dataset:isset, {name}, type string, load {name}; metadata:created_at {created_at}, updated_at {updated_at}{metadata}; sid:{sid}; rev:{rev};)',
+        ]
     }
 }
