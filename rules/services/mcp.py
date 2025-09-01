57c436b8cda201316705c0cbe16948171ce69833
Sébastien Lundi <slundi@stamus-networks.com>
2025-09-01 17:11:41 +0200

    feat(rules): rule info for MCP

--- rules/services/mcp.py
+++ rules/services/mcp.py
@@ -3,7 +3,8 @@ from django.http import HttpRequest
 from pydantic import IPvAnyAddress, PositiveInt
 from rest_framework.request import Request
 
-from rules.messages.mcp import AlertMessage
+from rules.django_repository.rule import RuleRepository
+from rules.messages.mcp import AlertMessage, RuleMessage, RuleReferenceMessage
 from rules.es_graphs import ESEventsTail
 from rules.es_query import ESPaginator
 
@@ -53,7 +54,8 @@ class McpService:
                 protocol=line["_source"]["app_proto"],
                 category=line["_source"]["alert"]["category"],
                 community_id=line["_source"]["community_id"],
-            ) for line in raw["hits"]["hits"]
+            )
+            for line in raw["hits"]["hits"]
         ]
 
     def alert_list(
@@ -75,3 +77,19 @@ class McpService:
         """
         request = self._prepare_alert_list_request_object(start, end, ip, page, limit)
         return self._get_alert_list_results(request)
+
+    def rules(self, sids: list[int]) -> list[RuleMessage]:
+        repo = RuleRepository()
+        return [
+            RuleMessage(
+                sid=rule.sid,
+                # category=rule.category.name,
+                # category_description=rule.category.descr,
+                # category_source=rule.category__source.name,
+                message=rule.msg,
+                # hits=rule.hits,
+                references=[RuleReferenceMessage(**value) for value in rule.extract_rule_references()],
+                content=rule.ruleatversion_set.order_by("-version").first().content,
+            )
+            for rule in repo.rules(sids, with_rule_at_version=True, with_categories=True)
+        ]
