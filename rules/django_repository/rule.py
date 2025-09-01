1a63693eafebbef8412a0d771479ed02f4f03c17
Sébastien Lundi <slundi@stamus-networks.com>
2025-09-01 17:11:41 +0200

    feat(rules): rule info for MCP

---
+++ rules/django_repository/rule.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Iterable
+
+from rules.models import Rule
+
+
+class RuleRepositoryIterface(ABC):
+    @abstractmethod
+    def rules(
+        self, sids: list[int], with_rule_at_version: bool = False, with_categories: bool = False
+    ) -> Iterable[Rule]:
+        pass
+
+
+class RuleRepository(RuleRepositoryIterface):
+    def rules(
+        self, sids: list[int], with_rule_at_version: bool = False, with_categories: bool = False
+    ) -> Iterable[Rule]:
+        qs = Rule.objects.filter(sid__in=sids)
+        if with_categories:
+            qs = qs.select_related("category")
+        if with_rule_at_version:
+            qs = qs.prefetch_related("ruleatversion_set")
+        return qs.order_by("sid").iterator(chunk_size=128)
