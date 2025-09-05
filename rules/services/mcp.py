e7c25e016a2c0bab5f42fc1daf3444b33faf1e86
Sébastien Lundi <slundi@stamus-networks.com>
2025-09-05 09:43:22 +0200

    fix(rules): properly load rule reference

--- rules/services/mcp.py
+++ rules/services/mcp.py
@@ -149,7 +149,7 @@ class McpService:
                     sid=rule.sid,
                     message=rule.msg,
                     hits=hits,
-                    references=[RuleReferenceMessage(**value) for value in rule.extract_rule_references()],
+                    references=[RuleReferenceMessage(key=ref.key, value=ref.value, url=ref.url) for ref in rule.extract_rule_references()],
                     content=rule.ruleatversion_set.order_by("-version").first().content,
                     probes=probes,
                     timeline_data=timeline,
