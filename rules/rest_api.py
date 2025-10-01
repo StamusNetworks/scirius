2bec9f15ecba0ed0905f1abd76b2dac63720ab74
Eric Leblond <el@stamus-networks.com>
2025-10-01 12:05:57 +0200

    fix(rules): correctly handle integer values

--- rules/rest_api.py
+++ rules/rest_api.py
@@ -2355,7 +2355,8 @@ class _ESFieldsNoKeyword:
     NO_KEYWORD_FIELDS = (
         'src_port', 'dest_port', 'alert.signature_id', 'alert.severity',
         'http.length', 'http.status', 'vlan', 'geoip.provider.autonomous_system_number',
-        'tunnel.depth', 'flow.dest_port', 'flow.src_port'
+        'tunnel.depth', 'flow.dest_port', 'flow.src_port',
+        'stamus.incidents_id', 'stamus.asset_info.incident_id', 'stamus.offender_info.incident_id',
     )
 
 
