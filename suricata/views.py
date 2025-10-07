3f3639823e7b87a427a446ca1bf6420973d3d2f6
Sébastien Lundi <slundi@stamus-networks.com>
2025-10-07 09:29:10 +0200

    fix(rules): rules.models imports

--- suricata/views.py
+++ suricata/views.py
@@ -31,8 +31,7 @@ from scirius.utils import scirius_render
 from suricata.tables import RecurrentTaskTable  # noqa: F401
 from suricata.forms import SuricataForm, SuricataUpdateForm
 from suricata.models import Suricata
-from rules.models.model import SuppressedRuleAtVersion, dependencies_check, Rule
-from rules.models.user_action import UserAction
+from rules.models.model import SuppressedRuleAtVersion, dependencies_check, Rule, UserAction
 
 from django.conf import settings
 from rules.es_graphs import *  # noqa: F403, F401
