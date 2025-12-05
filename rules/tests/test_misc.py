5e69f29624eb816b8b1e7aa81bf3d2806ce03d4c
Sébastien Lundi <slundi@stamus-networks.com>
2025-12-05 11:41:49 +0100

    test(rules): migrate legacy system settings rest tests to pytest

--- rules/tests/test_misc.py
+++ rules/tests/test_misc.py
@@ -10,7 +10,6 @@ from django.db import models
 from rest_framework import status, mixins
 from rest_framework.test import APITestCase
 
-from rules.models.misc import SystemSettings
 from rules.models.model import Ruleset, Source, SourceUpdate
 from rules.rest_api import router
 from accounts.models import SciriusUser
@@ -251,39 +250,3 @@ class RestAPIChangelogTestCase(RestAPITestBase, APITestCase):
         response = self.http_get(reverse("sourceupdate-list"))
         self.assertEqual(response["results"][0]["source"], self.public_source.pk)
         self.assertEqual(response["results"][0]["data"]["updated"], data["updated"])
-
-
-class RestAPISystemSettingsTestCase(RestAPITestBase, APITestCase):
-    def setUp(self):
-        RestAPITestBase.setUp(self)
-        APITestCase.setUp(self)
-
-        self.system_settings = SystemSettings.objects.get_or_create(id=1)[0]
-
-    def test_001_system_get(self):
-        content = self.http_get(reverse("systemsettings"))
-        self.assertEqual("custom_elasticsearch" in content, True)
-        self.assertEqual("elasticsearch_url" in content, True)
-        self.assertEqual("http_proxy" in content, True)
-        self.assertEqual("use_http_proxy" in content, True)
-
-    def test_002_system_settings_update(self):
-        params = {
-            "use_http_proxy": True,
-            "http_proxy": "",
-            "https_proxy": "",
-            "custom_elasticsearch": False,
-            "elasticsearch_url": "http://elasticsearch:9200/",
-        }
-        content = self.http_patch(reverse("systemsettings"), params)
-        self.assertEqual(content["use_http_proxy"], True)
-
-        params = {
-            "use_http_proxy": False,
-            "http_proxy": "",
-            "https_proxy": "",
-            "custom_elasticsearch": False,
-            "elasticsearch_url": "http://elasticsearch:9200/",
-        }
-        content = self.http_put(reverse("systemsettings"), params)
-        self.assertEqual(content["use_http_proxy"], False)
