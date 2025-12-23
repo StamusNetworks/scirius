f5e2685aae6a193030b5afb45e24cb90f55e0570
Mathias Kurz <mkurz@stamus-networks.com>
2025-12-23 10:28:06 +0100

    chore(ui): make preview button available in CE

--- ui/app/components/PreviewUI/index.js
+++ ui/app/components/PreviewUI/index.js
@@ -1,1 +1,31 @@
-export default () => null;
+import React from 'react';
+
+import { PlayCircleOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import styled from 'styled-components';
+
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+`;
+
+const Icon = styled.span`
+  margin-right: 0.5rem;
+  display: inline-flex;
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`;
+
+export default () => (
+  <Menu.Item key="preview">
+    <Link href="/preview" target="_blank" rel="noopener noreferrer">
+      <Icon>
+        <PlayCircleOutlined />
+      </Icon>
+      Preview new UI
+    </Link>
+  </Menu.Item>
+);
