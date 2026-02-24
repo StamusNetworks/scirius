d9d5269b86f615ebb17225052a6d99870337c5e0
lanath <vvivier@stamus-networks.com>
2026-02-24 10:31:01 +0100

    chore: update default.nix to include pytest and build dependencies

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-  nativeBuildInputs = (with pkgs.buildPackages; [ python3 nodejs_20 commitlint ]) ++ (with pkgs.buildPackages; with python311Packages ;[ pip wheel python-ldap mypy ruff ]);
+  nativeBuildInputs = (with pkgs.buildPackages; [ python3 nodejs_20 commitlint ]) ++ (with pkgs.buildPackages; with python311Packages ;[ pip wheel python-ldap mypy ruff pytest ]);
+  buildInputs = with pkgs; [
+    openssh # openssh-client
+    gcc
+    gnumake # build-essential
+    openldap # libldap2-dev
+    cyrus_sasl # libsasl2-dev
+    libyaml # libyaml-dev
+    git
+    suricata
+  ];
 }
