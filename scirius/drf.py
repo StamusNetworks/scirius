c312b94eab57dbee270674eb29850ebd27ea5a99
Sébastien Lundi <slundi@stamus-networks.com>
2026-03-23 10:16:12 +0100

    fix(scirius): make orjson able to handle non standard DRF objects

--- scirius/drf.py
+++ scirius/drf.py
@@ -1,6 +1,12 @@
 import orjson
+import structlog
+
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
+from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
+
+
+logger = structlog.get_logger("django_structlog")
 
 
 class ORJSONRenderer(JSONRenderer):
@@ -8,7 +14,18 @@ class ORJSONRenderer(JSONRenderer):
         if data is None:
             return b""
 
-        return orjson.dumps(data, option=orjson.OPT_NAIVE_UTC | orjson.OPT_NON_STR_KEYS)
+        # Handle DRF wrapped objects
+        if isinstance(data, ReturnDict):
+            data = dict(data)
+        elif isinstance(data, ReturnList):
+            data = list(data)
+
+        try:
+            return orjson.dumps(data, option=orjson.OPT_NAIVE_UTC | orjson.OPT_NON_STR_KEYS)
+        except TypeError:
+            # fallback using the default behavior
+            logger.debug("Not JSON serializable", type=type(data))
+            return super().render(data, accepted_media_type, renderer_context)
 
 
 class ORJSONParser(JSONParser):
