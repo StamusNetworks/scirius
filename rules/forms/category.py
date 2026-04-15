22fb96516259c52b7541a429b0a3f1fb4968aaa4
Sébastien Lundi <slundi@stamus-networks.com>
2026-04-15 09:07:53 +0200

    refactor(rules): move UI transformation choice helpers out of model layer
    Category.get_transformation_choices() and Ruleset.get_transformation_choices()
    were static methods on model classes but only used by Django forms. Extract them
    as module-level functions in their respective form modules so the model layer has
    no dependency on UI concerns.

--- rules/forms/category.py
+++ rules/forms/category.py
@@ -18,17 +18,50 @@ You should have received a copy of the GNU General Public License
 along with Scirius.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
 from django import forms
 from django.conf import settings
 
-from rules.models.model import Category, Transformation
+from rules.models.model import Transformation
 
 from .common import BaseEditForm
 from .misc import RulesetPolicyEditPermForm
 from .source import RulesetChoiceForm
 
 MIDDLEWARE = __import__(settings.RULESET_MIDDLEWARE)
+
+
+def get_transformation_choices(key=Transformation.ACTION):
+    # Keys
+    ACTION = Transformation.ACTION
+    LATERAL = Transformation.LATERAL
+    TARGET = Transformation.TARGET
+
+    allowed_choices = []
+
+    if key == ACTION:
+        all_choices_set = set(Transformation.ActionTransfoType.get_choices())
+        allowed_choices = list(all_choices_set.intersection(set(settings.RULESET_TRANSFORMATIONS)))
+
+        A_BYPASS = Transformation.A_BYPASS
+        A_RULESET_DEFAULT = Transformation.A_RULESET_DEFAULT
+        A_NONE = Transformation.A_NONE
+
+        # TODO: move me in settings.RULESET_TRANSFORMATIONS
+        allowed_choices.append((A_BYPASS.value, A_BYPASS.name.title()))
+        allowed_choices.append((A_RULESET_DEFAULT.value, A_RULESET_DEFAULT.name.replace("_", " ").title()))
+        allowed_choices.append((A_NONE.value, A_NONE.name.title()))
+
+    if key == TARGET:
+        CAT_DEFAULT = Transformation.T_CAT_DEFAULT
+        allowed_choices = list(Transformation.TargetTransfoType.get_choices())
+        allowed_choices.remove((CAT_DEFAULT.value, CAT_DEFAULT.name.replace("_", " ").title()))
+
+    if key == LATERAL:
+        CAT_DEFAULT = Transformation.L_CAT_DEFAULT
+        allowed_choices = list(Transformation.LateralTransfoType.get_choices())
+        allowed_choices.remove((CAT_DEFAULT.value, CAT_DEFAULT.name.replace("_", " ").title()))
+
+    return tuple(sorted(allowed_choices))
 
 
 class CategoryTransformForm(RulesetPolicyEditPermForm, BaseEditForm, RulesetChoiceForm):
@@ -39,6 +72,6 @@ class CategoryTransformForm(RulesetPolicyEditPermForm, BaseEditForm, RulesetChoi
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["action"].choices = Category.get_transformation_choices(key=Transformation.ACTION)
-        self.fields["lateral"].choices = Category.get_transformation_choices(key=Transformation.LATERAL)
-        self.fields["target"].choices = Category.get_transformation_choices(key=Transformation.TARGET)
+        self.fields["action"].choices = get_transformation_choices(key=Transformation.ACTION)
+        self.fields["lateral"].choices = get_transformation_choices(key=Transformation.LATERAL)
+        self.fields["target"].choices = get_transformation_choices(key=Transformation.TARGET)
--- rules/models/model.py
+++ rules/models/model.py
@@ -2028,40 +2028,6 @@ class Category(models.Model, Transformable, Cache):
         else:
             CategoryTransformation(ruleset=ruleset, category_transformation=self, key=key.value, value=value.value).save()
 
-    @staticmethod
-    def get_transformation_choices(key=Transformation.ACTION):
-        # Keys
-        ACTION = Transformation.ACTION
-        LATERAL = Transformation.LATERAL
-        TARGET = Transformation.TARGET
-
-        allowed_choices = []
-
-        if key == ACTION:
-            all_choices_set = set(Transformation.ActionTransfoType.get_choices())
-            allowed_choices = list(all_choices_set.intersection(set(settings.RULESET_TRANSFORMATIONS)))
-
-            A_BYPASS = Transformation.A_BYPASS
-            A_RULESET_DEFAULT = Transformation.A_RULESET_DEFAULT
-            A_NONE = Transformation.A_NONE
-
-            # TODO: move me in settings.RULESET_TRANSFORMATIONS
-            allowed_choices.append((A_BYPASS.value, A_BYPASS.name.title()))
-            allowed_choices.append((A_RULESET_DEFAULT.value, A_RULESET_DEFAULT.name.replace("_", " ").title()))
-            allowed_choices.append((A_NONE.value, A_NONE.name.title()))
-
-        if key == TARGET:
-            CAT_DEFAULT = Transformation.T_CAT_DEFAULT
-            allowed_choices = list(Transformation.TargetTransfoType.get_choices())
-            allowed_choices.remove((CAT_DEFAULT.value, CAT_DEFAULT.name.replace("_", " ").title()))
-
-        if key == LATERAL:
-            CAT_DEFAULT = Transformation.L_CAT_DEFAULT
-            allowed_choices = list(Transformation.LateralTransfoType.get_choices())
-            allowed_choices.remove((CAT_DEFAULT.value, CAT_DEFAULT.name.replace("_", " ").title()))
-
-        return tuple(sorted(allowed_choices))
-
 
 class RangeCheckIntegerFields(models.Model):
     MIN = -2147483648
@@ -2758,44 +2724,6 @@ class Ruleset(models.Model, Transformable):
                 user_action_objects__object_id=self.pk,
             ).order_by("-date")
         return qs, user_has_all_ua
-
-    @staticmethod
-    def get_transformation_choices(key=Transformation.ACTION):
-        # Keys
-        ACTION = Transformation.ACTION
-        LATERAL = Transformation.LATERAL
-        TARGET = Transformation.TARGET
-
-        allowed_choices = []
-
-        if key == ACTION:
-            all_choices_set = set(Transformation.ActionTransfoType.get_choices())
-            allowed_choices = list(all_choices_set.intersection(set(settings.RULESET_TRANSFORMATIONS)))
-
-            A_BYPASS = Transformation.A_BYPASS
-            A_NONE = Transformation.A_NONE
-
-            # TODO: move me in settings.RULESET_TRANSFORMATIONS
-            allowed_choices.append((A_BYPASS.value, A_BYPASS.name.title()))
-            allowed_choices.append((A_NONE.value, A_NONE.name.title()))
-
-        if key == TARGET:
-            CAT_DEFAULT = Transformation.T_CAT_DEFAULT
-            RULESET_DEFAULT = Transformation.T_RULESET_DEFAULT
-
-            allowed_choices = list(Transformation.TargetTransfoType.get_choices())
-            allowed_choices.remove((CAT_DEFAULT.value, CAT_DEFAULT.name.replace("_", " ").title()))
-            allowed_choices.remove((RULESET_DEFAULT.value, RULESET_DEFAULT.name.replace("_", " ").title()))
-
-        if key == LATERAL:
-            CAT_DEFAULT = Transformation.L_CAT_DEFAULT
-            RULESET_DEFAULT = Transformation.L_RULESET_DEFAULT
-
-            allowed_choices = list(Transformation.LateralTransfoType.get_choices())
-            allowed_choices.remove((CAT_DEFAULT.value, CAT_DEFAULT.name.replace("_", " ").title()))
-            allowed_choices.remove((RULESET_DEFAULT.value, RULESET_DEFAULT.name.replace("_", " ").title()))
-
-        return tuple(allowed_choices)
 
     @staticmethod
     def get_icon():
